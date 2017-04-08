d12f42d8c6c76f3f88ef87bebcbb81decf960b2c
Luke Campagnola <lukec@alleninstitute.org>
2017-04-07 17:08:08 -0700

    Connection detection is getting closer..

--- connection_detection.py
+++ connection_detection.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 import numpy as np
 
 from neuroanalysis.spike_detection import detect_evoked_spike
-
+from neuroanalysis.stats import ragged_mean
 
 
 class Analyzer(object):
@@ -17,9 +17,9 @@ class Analyzer(object):
 
     def _attach(self, obj):
         attr = '_' + self.__class__.__name__
-        if hasattr(rec, attr):
-            raise TypeError("Object %s already has attached %s" % (rec, self.__class__.__name__))
-        setattr(rec, attr, self)
+        if hasattr(obj, attr):
+            raise TypeError("Object %s already has attached %s" % (obj, self.__class__.__name__))
+        setattr(obj, attr, self)
 
 
 class PulseStimAnalyzer(Analyzer):
@@ -54,7 +54,7 @@ class PulseStimAnalyzer(Analyzer):
             spike_info = []
             for i,pulse in enumerate(pulses):
                 on, off = pulse
-                spike = detect_evoked_spike(pre_rec, [on, off])
+                spike = detect_evoked_spike(self.rec, [on, off])
                 spike_info.append({'pulse_n': i, 'pulse_ind': on, 'spike': spike})
             self._evoked_spikes = spike_info
         return self._evoked_spikes
@@ -69,17 +69,18 @@ class MultiPatchAnalyzer(Analyzer):
         self._attach(srec)
         self.srec = srec
 
-    def get_spike_responses(pre_rec, post_recs):
-        """Given a presynaptic stimulus recording and a list of postsynaptic recordings,
-        return a structure containing evoked responses from all postsynaptic recordings.
+    def get_spike_responses(self, pre_rec, post_rec):
+        """Given a pre- and a postsynaptic recording, return a structure
+        containing evoked responses.
         
-            [ {pre_rec, post_rec, [{pulse_n, pulse_ind, spike, response}, ...]}, ... ]
+            [{pulse_n, pulse_ind, spike, response}, ...]
         
         """
         # detect presynaptic spikes
-        spikes = detect_patch_evoked_spikes(pre_rec)
+        pulse_stim = PulseStimAnalyzer.get(pre_rec)
+        spikes = pulse_stim.evoked_spikes()
         
-        # Select ranges to extract from postsynaptic recordings
+        # Select ranges to extract from postsynaptic recording
         for i,pulse in enumerate(spikes):
             spike = pulse['spike']
             if spike is None:
@@ -96,17 +97,11 @@ class MultiPatchAnalyzer(Analyzer):
                     continue
                 pulse['rec_stop'] = min(pulse['rec_stop'], pulse2['spike']['rise_index'])
                 break
-        
-        result = []
-        
-        
-        raise Exception()
-        #for rec in post_recs:
-            #result.append(deepcopy(spikes))
-            #for i,pulse in enumerate(result[-1]
             
+            # Extract data from postsynaptic recording
+            pulse['response'] = post_rec['primary'].data[pulse['rec_start']:pulse['rec_stop']]
         
-        return result
+        return spikes
 
 
 def detect_connections(expt_data, max_freq=50):
@@ -118,7 +113,8 @@ def detect_connections(expt_data, max_freq=50):
             ignore sweeps that fail QC
             collect data following each stimulus
             collect baseline noise data
-                (use noise.std() / sqrt(n) to estimate noise for averaged traces)
+                use noise.std() / sqrt(n) to estimate noise for averaged traces
+                use mode(noise) to correct for baseline
             collect mode / holding
     Average together all collected sweep chunks from the same cell, and of the same holding and clamp mode
         all pulses
@@ -132,14 +128,31 @@ def detect_connections(expt_data, max_freq=50):
     """
     
     # loop over all sweeps (presynaptic)
+    all_spikes = {}
     for srec in expt_data.contents:
-        print srec
+        mp_analyzer = MultiPatchAnalyzer(srec)
+        
         for pre_rec in srec.recordings:
-            print "   ", pre_rec
+            pre_id = pre_rec.device_id
+            all_spikes.setdefault(pre_id, {})
             # todo: ignore sweeps with high induction frequency
             
-            post_recs = srec.recordings[:]
-            post_recs.remove(pre_rec)
+            for post_rec in srec.recordings:
+                if post_rec is pre_rec:
+                    continue
+                post_id = post_rec.device_id
+                all_spikes[pre_id].setdefault(post_id, [])
+                spikes = {
+                    'spikes': mp_analyzer.get_spike_responses(pre_rec, post_rec),
+                    'pre_rec': pre_rec,
+                    'post_rec': post_rec,
+                }
+                all_spikes[pre_id][post_id].append(spikes)
+
+                # breadcrumbs
+                #spikes['sync_rec'] = srec
+                
+    return all_spikes
 
 
 if __name__ == '__main__':
@@ -159,4 +172,35 @@ if __name__ == '__main__':
     expt_file = sys.argv[1]
     expt = MiesNwb(expt_file)
     
-    detect_connections(expt)
+    all_spikes = detect_connections(expt)
+
+    from neuroanalysis.ui.plot_grid import PlotGrid
+    
+    n_devs = len(all_spikes)
+    plots = PlotGrid()
+    plots.set_shape(n_devs, n_devs)
+    plots.show() 
+    
+    for i, dev1 in enumerate(all_spikes):
+        for j, dev2 in enumerate(all_spikes):
+            if dev1 == dev2:
+                continue
+            responses = []
+            for rec in all_spikes[dev1][dev2]:
+                
+                # do filtering here:
+                pre_rec = rec['pre_rec']
+                post_rec = rec['post_rec']
+                if post_rec.clamp_mode != 'vc':
+                    continue
+                stim_name = pre_rec.meta['stim_name']
+                if '20Hz' not in stim_name:
+                    continue
+                
+                
+                for spike in rec['spikes']:
+                    if spike['spike'] is None:
+                        continue
+                    responses.append(spike['response'])
+            avg = ragged_mean(responses, method='clip')
+            plots[i,j].plot(avg)
