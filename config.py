f8007f6cfd4a314c4073a7307932bb8e0d30a6cd
Luke Campagnola <ausqualius@yahoo.com>
2017-10-06 15:13:02 -0700

    Make default rig name invalid

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ synphys_db_host: "postgresql://postgres:xxxxx@10.128.38.98"
 synphys_db: "synphys"
 synphys_data: "/path/to/server/synphys_data"
 cache_path: "cache"
-rig_name: 'MP2'
+rig_name: 'MP_'
 n_headstages: 8
 raw_data_paths:
     - '/home/luke/mnt/backup_server/MP1_backup/D_drive/Steph/'
