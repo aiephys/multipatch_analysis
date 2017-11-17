08964542bf838052621b1f0940c0ced262e7246c
Corinne Teeter <corinnet@alleninstitute.org>
2017-11-17 15:37:56 -0800

    averaging implimented

--- PSP_amp_vs_time.py
+++ PSP_amp_vs_time.py
@@ -28,12 +28,62 @@ from neuroanalysis.filter import bessel_filter
 from neuroanalysis.event_detection import exp_deconvolve
 
 
+def measure_amp(trace, min_or_max, baseline=(6e-3, 8e-3), response=(13e-3, 17e-3)):
+    '''get the max or min of the data in the trace object and subtract out the baseline
+    at the specified times
+    '''
+    baseline = trace.time_slice(*baseline).data.mean()
+    if min_or_max=='max':
+        peak = trace.time_slice(*response).data.max()
+    elif min_or_max=='min':
+        peak = trace.time_slice(*response).data.min()
+    else:
+        raise Exception('Are you looking for min or max')
+    return peak - baseline
+
+def bin_data(time_list, data_list, bin_size=10):
+    '''bins time series data in time bins with a specified size.
+    Time must be bigger than 0.
+    '''
+    max_time=max([max(tt) for tt in time_list])
+    if min([min(tt) for tt in time_list])<0: 
+        raise Exception('time values should not be negative')
+    if max_time<bin_size:
+        raise Exception('bin size is bigger than max time')
+    time_bins=np.arange(0, max_time+bin_size, bin_size) # this could potentially be broken depending on what the bin_size is
+    if time_bins[-1] < max_time:
+        raise Exception('Your largest time bin is less than max time.  Tweak your bin size.') 
+    time_bin_middle=np.mean(np.array([np.append(time_bins, 0), np.append(0, time_bins)]), axis=0)[1:-1] 
+    data_in_bins=[np.array([]) for ii in range(len(time_bin_middle))] #initialize a data structure to receive data in bins
+    for tt, ff in zip(time_list, data_list):
+        assert len(tt)==len(ff)
+        digits=np.digitize(tt, time_bins)-1 #digitize will assign values less than smalled timebin to a 0 index so -1 is used here
+        for ii in range(len(digits)):
+            data_in_bins[digits[ii]]=np.append(data_in_bins[digits[ii]], ff[ii])
+
+    return data_in_bins, time_bins, time_bin_middle
+
+def test_bin_data():
+    '''meant to tests the bin_data() module but really just using a simple input so one can watch what is happening
+    '''
+    time_list=[np.array([1,2,3,4,5]), np.array([0, 3, 4.5]), np.array([1.1, 1.2, 2.3, 2.5, 2.8, 4, 4.3, 4.9])]
+    data_list=[np.array([1,2,3,4,5]), np.array([6,7,8]), np.array([9,10,11,12,13,14,15, 16])]
+    data, time_bins, time_mid_points=bin_data(time_list, data_list, bin_size=2.1)
+
+def average_via_bins(time_list, data_list, bin_size=10):
+    '''takes list of time arrays and corresponding list of data arrays and returns the average
+    by placing the data in time bins
+    '''
+    data,time_bins, time_bin_middle=bin_data(time_list, data_list, bin_size)
+    average_data=[]
+    for bins in data:
+        average_data.append(np.mean(bins))
+    assert len(average_data)==len(time_bin_middle), "data length doesn't match time length"
+    return time_bin_middle, average_data
+    
+
 if __name__ == '__main__':
     app = pg.mkQApp()
-    #pg.dbg()
-    
-    #expt_index = sys.argv[1]
-    #pre_id, post_id = map(int, sys.argv[2:4])
     
     # Load experiment index
     cache_file = 'expts_cache.pkl'
@@ -43,15 +93,14 @@ if __name__ == '__main__':
     for connection in expts.connection_summary():
         cells = connection['cells']
         expt = connection['expt']
-        if cells[0].cre_type == 'pvalb' and cells[1].cre_type == 'pvalb':
+        if cells[0].cre_type == 'sim1' and cells[1].cre_type == 'sim1':
             synapses.append((expt, cells[0].cell_id, cells[1].cell_id))
             
-    #expt = expts[expt_index]
-    
-    #analyzer = MultiPatchExperimentAnalyzer(expt.data)
-    #pulses = analyzer.get_evoked_responses(pre_id, post_id, clamp_mode='ic', pulse_ids=[0])
-    p = pg.plot()
-    
+    p = pg.plot(labels={'left': 'peak of synaptic deflection (mV)', 'bottom': 'time since first recorded synapse (s)', 'top':'sim1 to sim1 connections'})    
+    a=pg.plot(labels={'top':'average base-line subtraced first pluse synaptic deflection (sim1 to sim1)', 'bottom': 'time (s)', 'left':'voltage (mV)'}) 
+
+    filtered=[]
+    time_list=[]
     for i,syn in enumerate(synapses):
         expt, pre_id, post_id = syn
         analyzer = DynamicsAnalyzer(expt, pre_id, post_id, align_to='spike')
@@ -61,48 +110,14 @@ if __name__ == '__main__':
         if len(responses) == 0:
             print("Skipping %s %d %d; no responses" % (expt.uid, pre_id, post_id))
             continue
-        # collect all events
-        #responses = analyzer.all_events
-        
-        n_responses = len(responses)
-        
-        # do exponential deconvolution on all responses
-        deconv = TraceList()
-#        grid1 = PlotGrid()
-#        grid1.set_shape(2, 1)
-        for j in range(n_responses):
-            r = responses.responses[j]
-#            grid1[0, 0].plot(r.time_values, r.data)
-            
-            filt = bessel_filter(r - np.median(r.time_slice(0, 10e-3).data), 300.)
-            responses.responses[j] = filt
-            
-            dec = exp_deconvolve(r, 15e-3)
-            baseline = np.median(dec.data[:100])
-            r2 = bessel_filter(dec-baseline, 300.)
-#            grid1[1, 0].plot(r2.time_values, r2.data)
-            
-            deconv.append(r2)
-        
-#        grid1.show()
-        
-    
-        def measure_amp(trace, min_or_max, baseline=(6e-3, 8e-3), response=(13e-3, 17e-3)):
-            baseline = trace.time_slice(*baseline).data.mean()
-            if min_or_max=='max':
-                peak = trace.time_slice(*response).data.max()
-            elif min_or_max=='min':
-                peak = trace.time_slice(*response).data.min()
-            else:
-                raise Exception('Are you looking for min or max')
-            return peak - baseline
-    
-    
-        average = responses.bsub_mean()
+
+        # figure out whether the trough or peak of the average synaptic trace is bigger and if that corresponds to the synapse type.  
+        # i.e. if it is an excitatory synapse we would expect the max defection to be negative
+        average = responses.bsub_mean() #returns average synaptic response with the average baseline subtracted
+        a.plot(average.time_values, average.data) #plot average of first pulse of individual synapses
         max_peak = measure_amp(average, min_or_max='max', baseline=(6e-3, 8e-3), response=(12e-3, 16e-3))
         min_peak = measure_amp(average, min_or_max='min', baseline=(6e-3, 8e-3), response=(12e-3, 16e-3))
-        
-        max_min = "max" if abs(max_peak)> abs(min_peak) else "min"
+        max_min = "max" if abs(max_peak)> abs(min_peak) else "min"  #find whether the peak or trough is larger
         wrong_synapse_type_flag = False
         if max_min == "min" and expt.cells[pre_id].cre_type in EXCITATORY_CRE_TYPES: 
             print ("Whoa this synapse looks like a inhibitory when cre line would say it should be excitatory!!!" )  
@@ -110,31 +125,35 @@ if __name__ == '__main__':
         if max_min == "max" and expt.cells[pre_id].cre_type in INHIBITORY_CRE_TYPES: 
             print ("Whoa this synapse looks like a excitatory when cre line would say it should be inhibitory!!!" )    
             wrong_synapse_type_flag = True  
+        
+        # find the peak or trough of every potential event and plot their amplitude over time of the experiment
         peak=[]
         base=[]
         time=[]
-        ordered=sorted(responses.responses, key=lambda rr:rr.start_time)
-        for ii in range(len(responses)):
-            rr=ordered[ii]
+        ordered=sorted(responses.responses, key=lambda rr:rr.start_time) #order the traces by time during the experiment
+        for rr in ordered:
             peak.append(measure_amp(rr, min_or_max=max_min, baseline=(6e-3, 8e-3), response=(12e-3, 16e-3)))
             base.append(measure_amp(rr, min_or_max=max_min, baseline=(0e-3, 2e-3), response=(6e-3, 10e-3)))
             time.append(rr.start_time)
         mean_base=np.mean(base)
-        time = np.array(time) - time[0]
+        time=np.array(time) - time[0]
+        time_list.append(time)
         peak_minus_base_average=np.array(peak)-mean_base
-        filtered=ndi.gaussian_filter(peak_minus_base_average, 2)    
+        filtered.append(ndi.gaussian_filter(peak_minus_base_average, 2))    
         
-#        p.plot(time, peak)
-#        p.plot(time, base, pen='r')
-#        p.plot(time, peak_minus_base_average, pen='g')
         if wrong_synapse_type_flag == False:
-            p.plot(time, filtered, pen=(i, len(synapses)*1.3))
+            p.plot(time, filtered[-1], pen=(i, len(synapses)*1.3))
         elif wrong_synapse_type_flag == True:
-            p.plot(time, filtered, pen='w')
-        
-        
-            
-        
+            p.plot(time, filtered[-1], pen=pg.mkPen(color=(i, len(synapses)*1.3),style=pg.QtCore.Qt.DashLine))
+                        
         app.processEvents()
     
+        
+    #because times of events aren't all at the same time, time binning is needed to get average time course
+    time_points, avg_data=average_via_bins(time_list, filtered, bin_size=60)
+    p.plot(time_points, avg_data, pen=pg.mkPen(color='w', width=5)) #plots average of the data
+    
+
+    app.processEvents()    
+    pg.QtGui.QApplication.exec_()    
     
