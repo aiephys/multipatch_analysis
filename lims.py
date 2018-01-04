2604a16db195984c3751c1b6aee3ac27fe75a680
Luke Campagnola <lukec@alleninstitute.org>
2018-01-04 12:57:56 -0800

    minor fix

--- lims.py
+++ lims.py
@@ -189,6 +189,8 @@ def cell_cluster_data_paths(cluster_id):
 
 def specimen_metadata(spec_id):
     recs = lims.query("select data from specimen_metadata where specimen_id=%d" % spec_id)
+    if len(recs) == 0:
+        return None
     meta = recs[0]['data']
     if meta == '':
         return None
