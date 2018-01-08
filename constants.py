c4aeef73b7d737fcd98e2aa9bc7d4a586bc60fe9
Luke Campagnola <lukec@alleninstitute.org>
2018-01-08 09:52:21 -0800

    genotype - driver - color mapping is working nicely

--- constants.py
+++ constants.py
@@ -5,6 +5,70 @@ EXCITATORY_CRE_TYPES = ['tlx3', 'sim1', 'rorb', 'ntsr1', 'rbp4', 'ctgf', 'glt25d
 ALL_CRE_TYPES = INHIBITORY_CRE_TYPES + EXCITATORY_CRE_TYPES + ['unknown']
 
 
+FLUOROPHORES = {
+    'tdTomato': 'red',
+    'GFP': 'green',
+    'EGFP': 'green',
+    'AF488': 'green',
+    'Cascade Blue': 'blue',
+    'EYFP': 'yellow',
+    'ZsGreen': 'green',
+}
+
+
+ACSF_RECIPES = ["2mM Ca & Mg", "1.3mM Ca & 1mM Mg"]
+INTERNAL_RECIPES = ["Standard K-Gluc"]
+INTERNAL_DYES = ['Cascade Blue', 'AF488']
+
+
+# ----------------- Genotype Handling ----------------------------
+# The purpose of the code below is to be able to ask, given a mouse genotype string, 
+# what cell types are marked, and what reporter color are they marked with.
+
+# There are two approaches:
+# 1. Write down a list of all possible genotype strings and the associated 
+#    driver / reporter information. This is simple and transparent, but requires
+#    constant maintenance as we add more genotypes.
+# 2. Write down the complete list of drivers and reporters, and use some simple
+#    genetic modeling to predict what phenotype will be created by a particular
+#    genotype.
+# Method (2) is mostly implemented below, but encounters some complexity with the "genetic
+# modeling" aspect. This is surely resolvable, but for now we just use (1) for its
+# simplicity.
+
+# -------------------------------------------------------------------------------
+# Here is the simple method (1)
+
+# Each genotype string maps to a list of (driver,[reporters]) pairs
+from collections import OrderedDict
+GENOTYPES = OrderedDict([
+    ("Tlx3-Cre_PL56/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Vip-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'vip': ['EGFP']}),
+    ("Ntsr1-Cre_GN220/wt;Pvalb-T2A-FlpO/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'ntsr1': ['EGFP']}),
+    ("Tlx3-Cre_PL56/wt;Vip-IRES-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'vip': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Sst-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'sst': ['EGFP']}),
+    ("Tlx3-Cre_PL56/wt;Vip-IRES-FlpO/wt;Ai65F/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'vip': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Tlx3-Cre_PL56/wt;Pvalb-T2A-FlpO/wt;Ai65F/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Ntsr1-Cre_GN220/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'ntsr1': ['EGFP']}),
+    ("Sst-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'sst': ['EGFP']}),
+    ("Tlx3-Cre_PL56/wt;Vip-IRES-FlpO/wt;PhiC31-neo/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'vip': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Sim1-Cre_KJ18/wt;Sst-IRES-FlpO/wt;Ai65F/Ai65F;Ai139(TIT2L-GFP-ICL-TPT)/wt", {'sst': ['tdTomato'], 'sim1': ['EGFP', 'tdTomato']}),
+    ("Tlx3-Cre_PL56/wt;Sst-IRES-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'sst': ['tdTomato'], 'tlx3': ['EGFP']}),
+    ("Vip-IRES-Cre/wt;Sst-IRES-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'sst': ['tdTomato'], 'vip': ['EGFP']}),
+    ("Sim1-Cre_KJ18/wt;Pvalb-T2A-FlpO/wt;Snap25-LSL-F2A-GFP/wt;Ai65F/wt", {'pvalb': ['tdTomato'], 'sim1': ['EGFP']}),
+    ("Vip-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'pvalb': ['tdTomato'], 'vip': ['EGFP']}),
+    ("Sim1-Cre_KJ18/wt;Sst-IRES-FlpO/wt;Ai65F/wt;Ai139(TIT2L-GFP-ICL-TPT)/wt", {'sst': ['tdTomato'], 'sim1': ['EGFP', 'tdTomato']}),
+    ("Tlx3-Cre_PL56/wt;Sst-IRES-FlpO/wt;Ai65F/Ai65F;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'sst': ['tdTomato'], 'tlx3': ['EGFP']}),
+])    
+
+
+
+
+# -------------------------------------------------------------------------------
+# Here is the more general/complex method (2) that attempts to automatically parse
+# genotype strings (see also genotypes.py)
+
+
 # see: 
 # http://help.brain-map.org/download/attachments/2818171/Connectivity_Resources.pdf
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4365051/pdf/nihms665520.pdf
@@ -31,69 +95,54 @@ ALL_CRE_TYPES = INHIBITORY_CRE_TYPES + EXCITATORY_CRE_TYPES + ['unknown']
 
 
 REPORTER_LINES = {
-    'Ai2':                        ('',           'EYFP'),
-    'Ai3':                        ('',           'EYFP'),
-    'Ai6':                        ('',           'ZsGreen'),
-    'Ai9':                        ('',           'tdTomato'),
-    'Ai14':                       ('',           'tdTomato'),
-    'Ai14(RCL-tdT)':              ('?',          'tdTomato?'),
-    'Ai27':                       ('',           'hChR2(H134R)-tdTomato'),
-    'Ai31':                       ('',           'Syp-Emerald'),
-    'Ai32':                       ('',           'ChR2(H134R)-EYFP'),
-    'Ai34':                       ('',           'Syp-tdTomato'),
-    'Ai35':                       ('',           'Arch-EGFP-ER2'),
-    'Ai57(RCFL-Jaws)':            ('cre&flp',    'Jaws-GFP-ER2'),
-    'Ai62(TITL-tdT)':             ('cre&tTA',    'tdTomato'),
-    'Ai63(TIT-tdT)':              ('tTA',        'tdTomato'),
-    'Ai65(RCFL-tdT)':             ('cre&flp',    'tdTomato'),
-    'Ai65F':                      ('flp',        'tdTomato'),
-    'Ai66(RCRL-tdT)':             ('cre&dre',    'tdTomato'),
-    'Ai72(RCL-VSFPB)':            ('cre',        'VSFP-Butterfly 1.2'),
-    'Ai87(RCL-iGluSnFR)':         ('cre',        'iGluSnFR'),
-    'Ai95(RCL-GCaMP6f)':          ('cre',        'GCaMP6f'),
-    'Ai96(RCL-GCaMP6s)':          ('cre',        'GCaMP6s'),
-    'Ai62(TITL-tdT)':             ('cre&tTA',    'tdTomato'),
-    'Ai82(TITL-GFP)':             ('cre&tTA',    'EGFP'),
-    'Ai79(TITL-Jaws)':            ('cre&tTA',    'Jaws-GFP-ER2'),
-    'Ai93(TITL-GCaMP6f)':         ('cre&tTA',    'GCaMP6f'),
-    'Ai94(TITL-GCaMP6s)':         ('cre&tTA',    'GCaMP6s'),
-    'Ai92(TITL-YCX2.60)':         ('cre&tTA',    'YCX2.60'),
-    'Ai78(TITL-VSFPB)':           ('cre&tTA',    'VSFP-Butterfly 1.2'),
-    'Ai85(TITL-iGluSnFR)':        ('cre&tTA',    'iGluSnFR'),
-    'Ai139(TIT2L-GFP-ICL-TPT)-D': ('cre',        'EGFP+tdTomato'),
-    'Ai140(TIT2L-GFP-ICL-tTA2)':  ('cre',        'EGFP'),
+    'Ai2':                        ('',           ['EYFP']),
+    'Ai3':                        ('',           ['EYFP']),
+    'Ai6':                        ('',           ['ZsGreen']),
+    'Ai9':                        ('',           ['tdTomato']),
+    'Ai14':                       ('',           ['tdTomato']),
+    'Ai14(RCL-tdT)':              ('?',          ['tdTomato?']),
+    'Ai27':                       ('',           ['hChR2(H134R)','tdTomato']),
+    'Ai31':                       ('',           ['Syp','Emerald']),
+    'Ai32':                       ('',           ['ChR2(H134R)','EYFP']),
+    'Ai34':                       ('',           ['Syp','tdTomato']),
+    'Ai35':                       ('',           ['Arch','EGFP','ER2']),
+    'Ai57(RCFL-Jaws)':            ('cre&flp',    ['Jaws','GFP','ER2']),
+    'Ai62(TITL-tdT)':             ('cre&tTA',    ['tdTomato']),
+    'Ai63(TIT-tdT)':              ('tTA',        ['tdTomato']),
+    'Ai65(RCFL-tdT)':             ('cre&flp',    ['tdTomato']),
+    'Ai65F':                      ('flp',        ['tdTomato']),
+    'Ai66(RCRL-tdT)':             ('cre&dre',    ['tdTomato']),
+    'Ai72(RCL-VSFPB)':            ('cre',        ['VSFP','Butterfly 1.2']),
+    'Ai87(RCL-iGluSnFR)':         ('cre',        ['iGluSnFR']),
+    'Ai95(RCL-GCaMP6f)':          ('cre',        ['GCaMP6f']),
+    'Ai96(RCL-GCaMP6s)':          ('cre',        ['GCaMP6s']),
+    'Ai62(TITL-tdT)':             ('cre&tTA',    ['tdTomato']),
+    'Ai82(TITL-GFP)':             ('cre&tTA',    ['EGFP']),
+    'Ai79(TITL-Jaws)':            ('cre&tTA',    ['Jaws','GFP','ER2']),
+    'Ai93(TITL-GCaMP6f)':         ('cre&tTA',    ['GCaMP6f']),
+    'Ai94(TITL-GCaMP6s)':         ('cre&tTA',    ['GCaMP6s']),
+    'Ai92(TITL-YCX2.60)':         ('cre&tTA',    ['YCX2.60']),
+    'Ai78(TITL-VSFPB)':           ('cre&tTA',    ['VSFP','Butterfly 1.2']),
+    'Ai85(TITL-iGluSnFR)':        ('cre&tTA',    ['iGluSnFR']),
+    'Ai139(TIT2L-GFP-ICL-TPT)-D': ('cre',        ['EGFP','tdTomato']),
+    'Ai139(TIT2L-GFP-ICL-TPT)':   ('cre',        ['EGFP','tdTomato']),
+    'Ai140(TIT2L-GFP-ICL-tTA2)':  ('cre',        ['EGFP']),
+    'Snap25-LSL-F2A-GFP':         ('cre',        ['EGFP']),
 }
    
    
 DRIVER_LINES = {
-    'Rorb-T2A-tTA2':              ('cre,tTA',    'rorb'),
-    'Tlx3-Cre_PL56':              ('cre',        'tlx3'),
-    'Sim1-Cre_KJ18':              ('cre',        'sim1'),
-    'Ntsr1-Cre_GN220':            ('cre',        'ntsr1'),
-    'Chat-IRES-Cre-neo':          ('cre',        'chat'),
-    'Rbp4-Cre_KL100':             ('cre',        'rbp4'),
-    'Pvalb-IRES-Cre':             ('cre',        'pvalb'),
-    'Sst-IRES-Cre':               ('cre',        'sst'),
-    'Vip-IRES-Cre':               ('cre',        'vip'),
-    'Pvalb-T2A-FlpO':             ('flp',        'pvalb'),
-    'Sst-IRES-FlpO':              ('flp',        'sst'),
-    'Vip-IRES-FlpO':              ('flp',        'vip'),
+    'Rorb-T2A-tTA2':              (['cre','tTA'],  'rorb'),
+    'Tlx3-Cre_PL56':              (['cre'],        'tlx3'),
+    'Sim1-Cre_KJ18':              (['cre'],        'sim1'),
+    'Ntsr1-Cre_GN220':            (['cre'],        'ntsr1'),
+    'Chat-IRES-Cre-neo':          (['cre'],        'chat'),
+    'Rbp4-Cre_KL100':             (['cre'],        'rbp4'),
+    'Pvalb-IRES-Cre':             (['cre'],        'pvalb'),
+    'Sst-IRES-Cre':               (['cre'],        'sst'),
+    'Vip-IRES-Cre':               (['cre'],        'vip'),
+    'Pvalb-T2A-FlpO':             (['flp'],        'pvalb'),
+    'Sst-IRES-FlpO':              (['flp'],        'sst'),
+    'Vip-IRES-FlpO':              (['flp'],        'vip'),
 }
-
-
-FLUOROPHORES = {
-    'tdTomato': 'red',
-    'GFP': 'green',
-    'EGFP': 'green',
-    'AF488': 'green',
-    'Cascade Blue': 'blue',
-    'EYFP': 'yellow',
-    'ZsGreen': 'green',
-}
-
-
-
-ACSF_RECIPES = ["2mM Ca & Mg", "1.3mM Ca & 1mM Mg"]
-INTERNAL_RECIPES = ["Standard K-Gluc"]
-INTERNAL_DYES = ['Cascade Blue', 'AF488']
 
