2c53dab9fa71114ca55c9c07a5aea8c481c62070
ALLENINST\stephanies <stephanies@alleninstitute.org>
2018-01-12 16:29:25 -0800

    add slc17a8

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 ALL_LABELS = ['biocytin', 'af488', 'cascade_blue']
 HUMAN_LABELS = ['human_L2', 'human_L3', 'human_L4', 'human_L5', 'human_L6']
 INHIBITORY_CRE_TYPES = ['sst','pvalb', 'vip', 'ndnf', 'chat', 'htr3a', 'nos1', 'chrna2']
-EXCITATORY_CRE_TYPES = ['tlx3', 'sim1', 'rorb', 'ntsr1', 'rbp4', 'ctgf', 'glt25d2', 'L23pyr'] + HUMAN_LABELS
+EXCITATORY_CRE_TYPES = ['tlx3', 'sim1', 'rorb', 'ntsr1', 'rbp4', 'ctgf', 'glt25d2', 'L23pyr', 'slc17a8'] + HUMAN_LABELS
 ALL_CRE_TYPES = INHIBITORY_CRE_TYPES + EXCITATORY_CRE_TYPES + ['unknown']
 
 
