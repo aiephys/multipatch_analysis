24cc5396628657ca90d5c5b37b84085f906eb77b
Luke Campagnola <lukec@alleninstitute.org>
2018-01-19 14:27:52 -0800

    Add new Electrode class

--- multipatch_analysis/experiment_list.py
+++ multipatch_analysis/experiment_list.py
@@ -61,7 +61,7 @@ def indentation(line):
 class ExperimentList(object):
 
     def __init__(self, expts=None, cache=None):
-        self._cache_version = 6
+        self._cache_version = 7
         self._cache = cache
         self._expts = []
         self._expts_by_datetime = {}
