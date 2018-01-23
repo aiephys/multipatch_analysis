a0761228b46da5a89e94100439f906d7691ea374
Luke Campagnola <lukec@alleninstitute.org>
2018-01-22 17:26:26 -0800

    parallelize db submission

--- tools/import_to_database.py
+++ tools/import_to_database.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 import os, sys, time, glob, argparse
-import pyqtgraph.multiprocess as mp
 import multiprocessing
 
 import pyqtgraph as pg
@@ -12,75 +11,78 @@ from multipatch_analysis.database import database
 from multipatch_analysis import config, synphys_cache, experiment_list
 
 
+all_expts = experiment_list.cached_experiments()
+
+
+def submit_expt(expt_id):
+    try:
+        expt = all_expts[expt_id]
+        start = time.time()
+        
+        slice_dir = expt.slice_dir
+        print("submit slice:", slice_dir)
+        sub = SliceSubmission(slice_dir)
+        if sub.submitted():
+            print("   already in DB")
+        else:
+            sub.submit()
+        
+        print("    %g sec" % (time.time()-start))
+        start = time.time()
+        
+        print("submit experiment:")
+        print("    ", expt)
+        sub = ExperimentDBSubmission(expt)
+        if sub.submitted():
+            print("   already in DB")
+        else:
+            sub.submit()
+
+        print("    %g sec" % (time.time()-start))
+    except Exception:
+        print(">>>> %d Error importing experiment %s" % (os.getpid(), expt.uid))
+        sys.excepthook(*sys.exc_info())
+        print("<<<< %s" % expt.uid)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--limit', type=int, default=None)
     parser.add_argument('--local', action='store_true', default=False)
+    parser.add_argument('--workers', type=int, default=6)
+    parser.add_argument('--uid', type=str, default=None)
     args, extra = parser.parse_known_args(sys.argv[1:])
     
-    
-    all_expts = list(experiment_list.cached_experiments())
-    
-    # Just a dirty trick to give a wider variety of experiments when we are testing
-    # on a small subset
-    import random
-    random.seed(0)
-    random.shuffle(all_expts)
-    
-    if args.limit > 0:
-        selected_expts = all_expts[:args.limit]
+    if args.uid is not None:
+        selected_expts = [all_expts[uid] for uid in args.uid.split(',')]
     else:
-        selected_expts = all_expts
+        # Just a dirty trick to give a wider variety of experiments when we are testing
+        # on a small subset
+        import random
+        random.seed(0)
+        shuffled = list(all_expts)
+        random.shuffle(shuffled)
+        
+        if args.limit > 0:
+            selected_expts = shuffled[:args.limit]
+        else:
+            selected_expts = shuffled
         
     print("Found %d cached experiments, will import %d." % 
           (len(all_expts), len(selected_expts)))
+    print([ex.uid for ex in selected_expts])
     
-    for i, expt in enumerate(selected_expts):
-        try:
-            start = time.time()
-            
-            slice_dir = expt.slice_dir
-            print("submit slice:", slice_dir)
-            sub = SliceSubmission(slice_dir)
-            if sub.submitted():
-                print("   already in DB")
-            else:
-                sub.submit()
-            
-            print("    %g sec" % (time.time()-start))
-            start = time.time()
-            
-            print("submit experiment:")
-            print("    ", expt)
-            sub = ExperimentDBSubmission(expt)
-            if sub.submitted():
-                print("   already in DB")
-            else:
+    if args.local is True:
+        for i, expt in enumerate(selected_expts):
+            submit_expt(expt.uid)
+    else:
+        ids = [expt.uid for expt in selected_expts]
 
-                if '--local' in sys.argv:
-                    sub.submit()
-                    
-                else:
-                    # Do experiment submission in a subprocess to control memory
-                    # usage.
-                    proc = mp.Process(pyqtapis={'QString': 2, 'QVariant': 2})
-                    try:
-                        rxl = proc._import('multipatch_analysis.experiment_list')
-                        all_expts = rxl.cached_experiments()
-                        expt = all_expts[expt.uid]
-
-                        rsub = proc._import('multipatch_analysis.database.submission')
-                        sub = rsub.ExperimentDBSubmission(expt)
-                        fn = sub.submit(_timeout=None)
-                        
-                    finally:
-                        proc.join()
-
-            print("    %g sec" % (time.time()-start))
-        except Exception:
-            print("=============================")
-            print("failed:\n  %s" % (expt))
-            #raise
-            sys.excepthook(*sys.exc_info())
+        # Dispose DB engine before forking, otherwise child processes will
+        # inherit and muck with the same connections. See:
+        # http://docs.sqlalchemy.org/en/rel_1_0/faq/connections.html#how-do-i-use-engines-connections-sessions-with-python-multiprocessing-or-os-fork
+        database.engine.dispose()
         
-    
+        pool = multiprocessing.Pool(processes=args.workers, maxtasksperchild=1)
+        pool.map(submit_expt, ids)
+
