2e6761b032edb2dcdfc9008f92c93890193d9236
ALLENINST\stephanies <stephanies@alleninstitute.org>
2018-01-30 10:55:25 -0800

    Make analyses a bit more user friendly

--- analyses/first_pulse_feature.py
+++ analyses/first_pulse_feature.py
@@ -1,13 +1,16 @@
 import pyqtgraph as pg
 import numpy as np
 import csv
-from multipatch_analysis.experiment_list import ExperimentList
-from manuscript_figures import get_response, get_amplitude, response_filter, feature_anova, write_cache, trace_plot, colors_human, colors_mouse, fail_rate
+import sys
+import argparse
+from multipatch_analysis.experiment_list import cached_experiments
+from manuscript_figures import get_response, get_amplitude, response_filter, feature_anova, write_cache, trace_plot, \
+    colors_human, colors_mouse, fail_rate, pulse_qc
 from synapse_comparison import load_cache, summary_plot_pulse
 from neuroanalysis.data import TraceList
 from neuroanalysis.ui.plot_grid import PlotGrid
 from multipatch_analysis.connection_detection import fit_psp
-from rep_connections import ee_connections, human_connections
+from rep_connections import ee_connections, human_connections, no_include, all_connections
 from multipatch_analysis.synaptic_dynamics import DynamicsAnalyzer
 
 app = pg.mkQApp()
@@ -15,23 +18,54 @@ pg.dbg()
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
 
-expt_cache = 'C:/Users/Stephanies/multipatch_analysis/tools/expts_cache.pkl'
-all_expts = ExperimentList(cache=expt_cache)
-connections = human_connections
-color_palette = colors_human
-connection_types = connections.keys()
+parser = argparse.ArgumentParser(description='Enter organism and type of connection you"d like to analyze ex: mouse ee (all mouse excitatory-'
+                'excitatory). Alternatively enter a cre-type connection ex: sim1-sim1')
+parser.add_argument('--organism', dest='organism', help='Select mouse or human')
+parser.add_argument('--connection', dest='connection', help='Specify connections to analyze')
+args = vars(parser.parse_args(sys.argv[1:]))
 
-calcium = None
-age = None
+all_expts = cached_experiments()
+
+if args['organism'] == 'mouse':
+    color_palette = colors_mouse
+    calcium = 'high'
+    age = '40-60'
+    connection = args['connection']
+    if connection == 'ee':
+        connection_types = ee_connections.keys()
+    elif connection == 'all':
+        connection_types = all_connections.keys()
+    elif len(connection.split('-')) == 2:
+        c_type = connection.split('-')
+        if c_type[0] == '2/3':
+            pre_type = ('2/3', 'unknown')
+        else:
+            pre_type = (None, c_type[0])
+        if c_type[1] == '2/3':
+            post_type = ('2/3', 'unknown')
+        else:
+            post_type = (None, c_type[0])
+        connection_types = [(pre_type, post_type)]
+elif args['organism'] == 'human':
+    color_palette = colors_human
+    calcium = None
+    age = None
+    connection = args['connection']
+    if connection == 'ee':
+        connection_types = human_connections.keys()
+    else:
+        c_type = connection.split('-')
+        connection_types = [((c_type[0], 'unknown'), (c_type[1], 'unknown'))]
+
 sweep_threshold = 5
 threshold = 0.03e-3
 scale_offset = (-20, -20)
 scale_anchor = (0.4, 1)
-
+qc_plot = pg.plot()
 grand_response = {}
 feature_plot = None
 synapse_plot = PlotGrid()
-synapse_plot.set_shape(len(connection_types), 2)
+synapse_plot.set_shape(len(connection_types), 1)
 synapse_plot.show()
 for c in range(len(connection_types)):
     cre_type = (connection_types[c][0][1], connection_types[c][1][1])
@@ -43,7 +77,9 @@ for c in range(len(connection_types)):
     synapse_plot[c, 0].addLegend()
     for expt in expt_list:
         for pre, post in expt.connections:
-            cre_check =  expt.cells[pre].cre_type == cre_type[0] and expt.cells[post].cre_type == cre_type[1]
+            if [expt.uid, pre, post] in no_include:
+                continue
+            cre_check = expt.cells[pre].cre_type == cre_type[0] and expt.cells[post].cre_type == cre_type[1]
             layer_check = expt.cells[pre].target_layer == target_layer[0] and expt.cells[post].target_layer == target_layer[1]
             if cre_check is True and layer_check is True:
                 pulse_response, artifact = get_response(expt, pre, post, type='pulse')
@@ -51,30 +87,34 @@ for c in range(len(connection_types)):
                     continue
                 response_subset = response_filter(pulse_response, freq_range=[0, 50], holding_range=[-68, -72], pulse=True)
                 if len(response_subset) >= sweep_threshold:
-                    avg_trace, avg_amp, amp_sign, peak_t = get_amplitude(response_subset)
-                    if amp_sign is '-':
-                        continue
-                    #grand_response[cre_type[0]]['fail_rate'].append(fail_rate(response_subset, '+', peak_t))
-                    psp_fits = fit_psp(avg_trace, sign=amp_sign, yoffset=0, amp=avg_amp, method='leastsq', fit_kws={})
-                    avg_trace.t0 = -(psp_fits.best_values['xoffset'] - 10e-3)
-                    grand_response[type[0]]['latency'].append(psp_fits.best_values['xoffset'] - 10e-3)
-                    grand_response[type[0]]['rise'].append(psp_fits.best_values['rise_time'])
-                    grand_response[type[0]]['trace'].append(avg_trace)
-                    grand_response[type[0]]['amp'].append(avg_amp)
-                    synapse_plot[c, 0].setTitle('First Pulse Response')
-                    if [expt.uid, pre, post] == connections[type]:
-                        trace_color = (255, 0, 255, 30)
-                    else:
-                        trace_color = (0, 0, 0, 30)
-                    trace_plot(avg_trace, trace_color, plot=synapse_plot[c, 0], x_range=[0, 27e-3])
-                    app.processEvents()
-                decay_response = response_filter(pulse_response, freq_range=[0, 20], holding_range=[-68, -72])
-                if len(response_subset) >= sweep_threshold:
-                    avg_trace, avg_amp, amp_sign, peak_t = get_amplitude(response_subset)
-                    if amp_sign is '-':
-                        continue
-                    psp_fits = fit_psp(avg_trace, sign=amp_sign, yoffset=0, amp=avg_amp, method='leastsq', fit_kws={})
-                    grand_response[type[0]]['decay'].append(psp_fits.best_values['decay_tau'])
+                    qc_plot.clear()
+                    qc_list = pulse_qc(response_subset, baseline=2.5, pulse=None, plot=qc_plot)
+                    if len(qc_list) >= sweep_threshold:
+                        avg_trace, avg_amp, amp_sign, peak_t = get_amplitude(qc_list)
+                        if amp_sign is '-':
+                            continue
+                        #grand_response[cre_type[0]]['fail_rate'].append(fail_rate(response_subset, '+', peak_t))
+                        psp_fits = fit_psp(avg_trace, sign=amp_sign, yoffset=0, amp=avg_amp, method='leastsq', fit_kws={})
+                        avg_trace.t0 = -(psp_fits.best_values['xoffset'] - 10e-3)
+                        grand_response[type[0]]['latency'].append(psp_fits.best_values['xoffset'] - 10e-3)
+                        grand_response[type[0]]['rise'].append(psp_fits.best_values['rise_time'])
+                        grand_response[type[0]]['trace'].append(avg_trace)
+                        grand_response[type[0]]['amp'].append(avg_amp)
+                        synapse_plot[c, 0].setTitle('First Pulse Response')
+                        if [expt.uid, pre, post] == all_connections[type]:
+                            trace_color = (255, 0, 255, 30)
+                        else:
+                            trace_color = (0, 0, 0, 30)
+                        trace_plot(avg_trace, trace_color, plot=synapse_plot[c, 0], x_range=[0, 27e-3])
+                        app.processEvents()
+                    decay_response = response_filter(pulse_response, freq_range=[0, 20], holding_range=[-68, -72])
+                    qc_list = pulse_qc(response_subset, baseline=2, pulse=None, plot=qc_plot)
+                    if len(qc_list) >= sweep_threshold:
+                        avg_trace, avg_amp, amp_sign, peak_t = get_amplitude(qc_list)
+                        if amp_sign is '-':
+                            continue
+                        psp_fits = fit_psp(avg_trace, sign=amp_sign, yoffset=0, amp=avg_amp, method='leastsq', fit_kws={})
+                        grand_response[type[0]]['decay'].append(psp_fits.best_values['decay_tau'])
     if len(grand_response[type[0]]['trace']) == 0:
         continue
     if len(grand_response[type[0]]['trace']) > 1:
@@ -91,14 +131,15 @@ for c in range(len(connection_types)):
     # synapse_plot[c, 1].plot(x, y, stepMode=True, fillLevel=0, brush='k')
     # synapse_plot[c, 1].setLabels(bottom=('Vm', 'V'))
     # synapse_plot[c, 1].setXRange(0, 2e-3)
-    feature_list = (grand_response[type[0]]['amp'], grand_response[type[0]]['latency'], grand_response[type[0]]['rise'])
+    feature_list = (grand_response[type[0]]['amp'], grand_response[type[0]]['latency'], grand_response[type[0]]['rise'],
+                    grand_response[type[0]]['decay'])
     grand_amp = np.mean(np.array(grand_response[type[0]]['amp']))
     grand_latency = np.mean(np.array(grand_response[type[0]]['latency']))
     grand_rise = np.mean(np.array(grand_response[type[0]]['rise']))
     grand_decay = np.mean(np.array(grand_response[type[0]]['decay']))
-    labels = (['Vm', 'V'], ['t', 's'], ['t', 's'])
-    feature_plot = summary_plot_pulse(grand_trace, feature_list,(grand_amp, grand_latency, grand_rise), labels,
-                                  ('Amplitude', 'Latency', 'Rise time'), c, plot=feature_plot,
+    labels = (['Vm', 'V'], ['t', 's'], ['t', 's'], ['t', 's'])
+    feature_plot = summary_plot_pulse(feature_list,(grand_amp, grand_latency, grand_rise, grand_decay), labels,
+                                  ('Amplitude', 'Latency', 'Rise time', 'Decay Tau'), c, grand_trace, plot=feature_plot,
                                   color=color, name=connection_types[c])
     if c == len(connection_types) - 1:
         x_scale = pg.ScaleBar(size=10e-3, suffix='s')
@@ -107,5 +148,4 @@ for c in range(len(connection_types)):
 amp_list = feature_anova('amp', grand_response)
 latency_list = feature_anova('latency', grand_response)
 rise_list = feature_anova('rise', grand_response)
-
-#csv.writer([amp_list, latency_list, rise_list], 'feature_summary_EE.pkl')
+decay_list = feature_anova('decay', grand_response)
