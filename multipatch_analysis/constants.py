9dce1aaf379f97e76caf2053658983530012ccc7
ALLENINST\stephanies <stephanies@alleninstitute.org>
2018-01-31 16:09:57 -0800

    ntsr1/sst

--- multipatch_analysis/constants.py
+++ multipatch_analysis/constants.py
@@ -74,7 +74,8 @@ GENOTYPES = OrderedDict([
     ("Slc17a8-IRES2-Cre/wt;Ai14(RCL-tdT)/wt", {'slc17a8': ['tdTomato']}),
     ("Ntsr1-Cre_GN220/wt;Vip-IRES-FlpO/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt;Ai65F/wt", {'vip': ['tdTomato'], 'ntsr1': ['EGFP']}),
     ("Sst-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/Ai65F", {'pvalb': ['tdTomato']}),
-    ("Sst-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt", {'pvalb': ['tdTomato']})
+    ("Sst-IRES-Cre/wt;Pvalb-T2A-FlpO/wt;Ai65F/wt", {'pvalb': ['tdTomato']}),
+    ("Ntsr1-Cre_GN220/wt;Sst-IRES-FlpO/wt;Ai65F/wt;Ai140(TIT2L-GFP-ICL-tTA2)/wt", {'sst': ['tdTomato'], 'ntsr1': ['EGFP']})
 ])    
 
 
