b116670859fb5cce294660a3a784d209d88885b4
Luke Campagnola <lukec@alleninstitute.org>
2018-02-09 11:26:31 -0800

    changed fig 3 inclusion criteria

--- analyses/manuscript_fig_3.py
+++ analyses/manuscript_fig_3.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import pyqtgraph as pg
 import numpy as np
 
+from neuroanalysis.data import TraceList
 import strength_analysis
 from multipatch_analysis.database import database as db
 
@@ -15,9 +16,16 @@ from multipatch_analysis.database import database as db
 if __name__ == '__main__':
     show_conns = [
         # (expt_uid, pre_cell, post_cell)
-        ("low signal, low noise", (1499277786.89, 1, 3)),
-        ("low signal, high noise", (1495833911.11, 1, 8)),
-        ("high signal, high noise", (1489441647.6, 8, 5)),
+        # ("low signal, low noise", (1499277786.89, 1, 3)),
+        # ("low signal, low noise", (1503953399.15, 1, 7)),
+        # ("low signal, high noise", (1495833911.11, 1, 8)),
+        # ("low signal, high noise", (1509566559.2, 7, 1)),
+        # ("high signal, high noise", (1489441647.6, 8, 5)),
+
+        # ("low signal, low noise", (1506537287.63, 7, 8)),
+        ("low signal, low noise", (1502312765.01, 1, 4)),
+        ("low signal, high noise", (1494969844.93, 6, 1)),
+        ("high signal, low noise", (1499725138.07, 7, 4)),
     ]
     
     pg.mkQApp()
@@ -39,12 +47,21 @@ if __name__ == '__main__':
     # filter
     mask = np.isfinite(conns['abs_deconv_base_amp_med'])
     filtered = conns[mask]
+
     # remove recordings with gain errors
     mask = filtered['abs_deconv_base_amp_med'] < 0.02
+
     # remove recordings likely to have high crosstalk
-    mask &= filtered['electrode_distance'] > 1
+    # mask &= filtered['electrode_distance'] > 1
+
     # remove recordings with low sample count
     mask &= filtered['n_samples'] > 50
+
+    typs = filtered['pre_cre_type']
+    mask &= typs == filtered['post_cre_type']
+
+    typ_mask = ((typs == 'sim1') | (typs == 'tlx3') | (typs == 'unknown') | (typs == 'rorb') | (typs == 'ntsr1'))
+    mask &= typ_mask
 
     filtered = filtered[mask]
 
@@ -55,16 +72,10 @@ if __name__ == '__main__':
     c_mask = filtered['synapse']
     u_mask = ~c_mask
 
-    signal = filtered['abs_deconv_amp_med']
-    background = filtered['abs_deconv_base_amp_med']
+    signal = filtered['deconv_amp_med']
+    background = filtered['deconv_base_amp_med']
 
-    def clicked(sp, pts):
-        print("-----------")
-        for pt in pts:
-            d = pt.data()
-            print(d['acq_timestamp'], d['pre_cell_id'], d['post_cell_id'])
     c_plot = scatter_plot.plot(background[c_mask], signal[c_mask], pen=None, symbol='d', symbolPen='k', symbolBrush=(0, 255, 255), symbolSize=10, data=filtered[c_mask])
-    c_plot.scatter.sigClicked.connect(clicked)
 
     u_plot = scatter_plot.plot(background[u_mask], signal[u_mask], pen=None, symbol='o', symbolPen=None, symbolBrush=(50, 50, 50, 80), symbolSize=4)
     u_plot.setZValue(-10)
@@ -73,16 +84,23 @@ if __name__ == '__main__':
     trace_plots = []
     hist_plots = []
 
-    # Handle selected individual connections
     session = db.Session()
-    p = pg.debug.Profiler(disabled=False, delayed=False)
-    for i, c in enumerate(show_conns):
-        name, key = c
-        ts, pre_id, post_id = key
+
+    def add_connection_plots(i, name, timestamp, pre_id, post_id):
+        global session, win, filtered
+        p = pg.debug.Profiler(disabled=False, delayed=False)
+        trace_plot = win.addPlot(i, 1)
+        trace_plots.append(trace_plot)
+        hist_plot = win.addPlot(i, 2)
+        hist_plots.append(hist_plot)
+        limit_plot = win.addPlot(i, 3)
+
         
         # Find this connection in the pair list
-        idx = np.argwhere((abs(filtered['acq_timestamp'] - ts) < 1) & (filtered['pre_cell_id'] == pre_id) & (filtered['post_cell_id'] == post_id))
-        assert idx.size == 1
+        idx = np.argwhere((abs(filtered['acq_timestamp'] - timestamp) < 1) & (filtered['pre_cell_id'] == pre_id) & (filtered['post_cell_id'] == post_id))
+        if idx.size == 0:
+            print("not in filtered connections")
+            return
         idx = idx[0,0]
         p()
 
@@ -90,14 +108,10 @@ if __name__ == '__main__':
         scatter_plot.plot([background[idx]], [signal[idx]], pen='k', symbol='o', size=10, symbolBrush='r', symbolPen=None)
             
         # Plot example traces and histograms
-        trace_plot = win.addPlot(i, 1)
-        trace_plots.append(trace_plot)
         trace_plot.setXLink(trace_plots[0])
         trace_plot.setYLink(trace_plots[0])
         trace_plot.setXRange(-10e-3, 20e-3)
 
-        hist_plot = win.addPlot(i, 2)
-        hist_plots.append(hist_plot)
         hist_plot.setXLink(hist_plots[0])
         
         pair = session.query(db.Pair).filter(db.Pair.id==filtered[idx]['pair_id']).all()[0]
@@ -112,7 +126,8 @@ if __name__ == '__main__':
         p()
         q = q.join(strength_analysis.PulseResponseStrength)
         q = q.filter(strength_analysis.PulseResponseStrength.id.in_(ids))
-
+        q = q.join(db.Recording, db.Recording.id==db.PulseResponse.recording_id).join(db.PatchClampRecording).join(db.MultiPatchProbe)
+        q = q.filter(db.MultiPatchProbe.induction_frequency < 100)
         # pre_cell = db.aliased(db.Cell)
         # post_cell = db.aliased(db.Cell)
         # q = q.join(db.Pair).join(db.Experiment).join(pre_cell, db.Pair.pre_cell_id==pre_cell.id).join(post_cell, db.Pair.post_cell_id==post_cell.id)
@@ -120,37 +135,61 @@ if __name__ == '__main__':
         # q = q.filter(pre_cell.ext_id==pre_id)
         # q = q.filter(post_cell.ext_id==post_id)
 
-        q = q.limit(50)
+        q = q.limit(100)
         recs = q.all()
         p()
         print(len(recs))
+        traces = []
         for rec in recs:
             result = strength_analysis.analyze_response_strength(rec, source='pulse_response', lpf=True, lowpass=2000,
                                                 remove_artifacts=False, bsub=True)
             trace = result['raw_trace']
             trace.t0 = -result['spike_time']
             base = np.median(trace.time_slice(-0.5e-3, 0.5e-3).data)
-            trace_plot.plot(trace.time_values, trace.data - base, pen=(0, 0, 0, 20))
-        
+            trace = trace - base
+            traces.append(trace)
+            trace_plot.plot(trace.time_values, trace.data, pen=(0, 0, 0, 20))
+
+        # plot average trace
+        mean = TraceList(traces).mean()
+        trace_plot.plot(mean.time_values, mean.data, pen={'color':'g', 'width': 2}, antialias=True)
+
+
         p("analyze_response_strength")
 
-        bins = np.arange(-0.0005, 0.002, 0.0001) 
-        field = 'pos_amp'
-        # bins = np.arange(-0.005, 0.02, 0.001) 
-        # field = 'pos_dec_amp'
+        # bins = np.arange(-0.0005, 0.002, 0.0001) 
+        # field = 'pos_amp'
+        bins = np.arange(-0.005, 0.02, 0.001) 
+        field = 'pos_dec_amp'
         hist_y, hist_bins = np.histogram(base_amps[field], bins=bins, density=True)
         hist_plot.plot(hist_bins, hist_y, stepMode=True, pen=None, brush=(200, 0, 0, 150), fillLevel=0)
         hist_y, hist_bins = np.histogram(amps[field], bins=bins, density=True)
-        hist_plot.plot(hist_bins, hist_y, stepMode=True, pen='k', brush=(0, 150, 150, 150), fillLevel=0)
+        hist_plot.plot(hist_bins, hist_y, stepMode=True, pen='k', brush=(0, 150, 150, 100), fillLevel=0)
         p()
 
         # Plot detectability analysis
-        limit_plot = win.addPlot(i, 3)
 
         # amps = np.arange(50e-6, 500e-6, 50e-6)
         # rtimes = np.arange(1e-3, 2e-3, 0.1e-3)
         # for amp in amps:
         #     for rtime in rtimes:
                 
+        pg.QtGui.QApplication.processEvents()
 
+    # Handle selected individual connections
+    next_row = 0
+    for name, key in show_conns:
+        ts, pre_id, post_id = key
+        add_connection_plots(next_row, name, ts, pre_id, post_id)
+        next_row += 1
 
+    def clicked(sp, pts):
+        global next_row
+        print("-----------")
+        for pt in pts:
+            d = pt.data()
+            print(d['acq_timestamp'], d['pre_cell_id'], d['post_cell_id'])
+            add_connection_plots(next_row, "", d['acq_timestamp'], d['pre_cell_id'], d['post_cell_id'])
+            next_row += 1
+
+    c_plot.scatter.sigClicked.connect(clicked)
