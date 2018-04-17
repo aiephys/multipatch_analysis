dada17c32c84ccfb875c408c5c13f0ced9c3e338
Luke Campagnola <lukec@alleninstitute.org>
2018-04-17 13:49:35 -0700

    Add sync_dir function

--- util/rig_backup.py
+++ util/rig_backup.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import os, sys
-from multipatch_analysis.util import sync_file
+from multipatch_analysis.util import sync_dir
 
 args = sys.argv[1:]
 if '--test' in args:
@@ -10,12 +10,4 @@ else:
     test = False
     
 source_path, dest_path = args
-assert os.path.isdir(source_path)
-assert os.path.isdir(dest_path)
-
-for path, subdirs, files in os.walk(source_path):
-    rel = os.path.relpath(path, source_path)
-    for fname in files:
-        src_file = os.path.join(path, fname)
-        dst_file = os.path.join(dest_path, rel, fname)
-        action = sync_file(src_file, dst_file, test=test)
+sync_dir(source_path, dest_path, test=test)
