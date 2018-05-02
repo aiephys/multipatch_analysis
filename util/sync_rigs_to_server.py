a12da9841a498fae64c9f1581ac34322b955c976
Luke Campagnola <lukec@alleninstitute.org>
2018-05-02 13:54:55 -0700

    more cleanup/code removal in sync script

--- util/sync_rigs_to_server.py
+++ util/sync_rigs_to_server.py
@@ -26,6 +26,11 @@ from multipatch_analysis.util import sync_file
 def sync_experiment(site_dir):
     """Synchronize all files for an experiment to the server.
 
+    Argument must be the path of an experiment _site_ folder. This will also cause
+    synchronization for the parent (slice) and grandparent (day) folders to ensure
+    that all slice images and metadata are copied. Sibling site and slice folders
+    will _not_ be copied.
+
     Return a list of changes made.
     """
     site_dh = getDirHandle(site_dir)
@@ -41,7 +46,7 @@ def sync_experiment(site_dir):
         # Decide how the top-level directory will be named on the remote server
         # (it may already be there from a previous slice/site, or the current
         # name may already be taken by another rig.)
-        server_expt_path = get_experiment_server_path(expt_dh)
+        server_expt_path = os.path.join(config.synphys_data, get_server_path(expt_dh))
         
         log("    using server path: %s" % server_expt_path)
         skipped += _sync_paths(expt_dh.name(), server_expt_path, changes)
@@ -56,8 +61,6 @@ def sync_experiment(site_dir):
         
         log("    Done; skipped %d files." % skipped)
         
-        # Leave a note about the source of this data
-        open(os.path.join(server_site_path, 'sync_source'), 'wb').write(site_dh.name())
     except Exception:
         err = traceback.format_exc()
         changes.append(('error', site_dh.name(), err))
@@ -105,100 +108,35 @@ def _sync_paths(source, target, changes):
                 log("    updt %s => %s" % (src_path, dst_path))
                 changes.append(('update', src_path, dst_path))
 
+    # Leave a note about the source of this data
+    open(os.path.join(target, 'sync_source'), 'wb').write(source)
     return skipped
 
 
-def get_experiment_server_path(dh):
-    server_path = config.synphys_data
-    acq_timestamp = dh.info()['__timestamp__']
-    
-    # First check the cache
-    cache = experiment_path_cache()
-    if acq_timestamp in cache:
-        return os.path.join(server_path, cache[acq_timestamp])
-    
-    # We have not already submitted a site from this experiment folder;
-    # look for a suitable new directory name on the server
-    expt_base_name = dh.shortName().split('_')[0]
-    expt_dirs = set(os.listdir(server_path))
-    i = 0
-    while True:
-        expt_name = expt_base_name + '_%03d' % i
-        if expt_name not in expt_dirs:
-            break
-        i += 1
-    server_expt_path = os.path.join(server_path, expt_name)
-    assert not os.path.exists(server_expt_path)
-    
-    os.mkdir(server_expt_path)
-    try:
-        dh = getDirHandle(server_expt_path)
-        # temporarily mark with timestamp; should be overwritten later.
-        dh.setInfo(__timestamp__=acq_timestamp)
-    except Exception:
-        if os.path.exists(server_expt_path):
-            shutil.rmtree(server_expt_path)
-        raise
-    
-    cache[acq_timestamp] = expt_name
-    write_expt_path_cache()
-    
-    return server_expt_path
-
-
-_expt_path_cache = None
-def experiment_path_cache():
-    """The server contains a pickled dictionary mapping expt_uid:path
+def get_server_path(dh):
+    """Given a directory handle to an experiment storage folder on a rig,
+    return the path to the corresponding location on the server, relative
+    to the server's root storage path.
     """
-    global _expt_path_cache
-    if _expt_path_cache is None:
-        cache_file = os.path.join(config.synphys_data, 'experiment_path_cache.pkl')
-        if os.path.isfile(cache_file):
-            try:
-                _expt_path_cache = pickle.load(open(cache_file, 'rb'))
-            except Exception:
-                print("Error loading experiment path cache; will regenerate:")
-                sys.excepthook(*sys.exc_info())
-                generate_expt_path_cache()
-        else:
-            generate_expt_path_cache()
-    return _expt_path_cache
+    # Find parent with dirType='Day'
+    root = dh
+    while True:
+        if root.info().get('dirType') == 'Day':
+            break
+        parent = root.parent()
+        if parent is root:
+            raise Exception("Can't find the experiment root folder for %s" % dh.name())
+        root = parent
+    
+    root_ts = root.info()['__timestamp__']
+    return os.path.join('%0.3f'%root_ts, dh.name(relativeTo=root))
 
-
-def generate_expt_path_cache():
-    global _expt_path_cache
-    _expt_path_cache = {}
-    root = getDirHandle(config.synphys_data)
-    for f in root.ls():
-        if 'recycle' in f.lower():
-            continue
-        dh = root[f]
-        if not dh.isDir():
-            continue
-        try:
-            acq_timestamp = dh.info()['__timestamp__']
-        except KeyError:
-            print("NO TIMESTAMP:", dh.name())
-            sys.exit(-1)
-        if acq_timestamp in _expt_path_cache:
-            raise Exception("timestamp %s appears twice in synphys data!!" % acq_timestamp)
-        _expt_path_cache[acq_timestamp] = dh.name(relativeTo=root)
-    write_expt_path_cache()
-    
-    
-def write_expt_path_cache():
-    global _expt_path_cache
-    cache_file = os.path.join(config.synphys_data, 'experiment_path_cache.pkl')
-    tmp = cache_file+'.tmp'
-    pickle.dump(_expt_path_cache, open(tmp, 'wb'))
-    os.rename(tmp, cache_file)
-    
 
 def find_all_sites(root):
     sites = glob.glob(os.path.join(root, '*', 'slice_*', 'site_*'))
     sites.sort(reverse=True)
     return sites
-    
+
 
 def sync_all():
     """Synchronize all known rig data paths to the server
@@ -218,13 +156,15 @@ def sync_all():
             paths = find_all_sites(data_path)
 
             # synchronize files for each experiment to the server
-            log.extend(sync_paths(paths))
+            log.extend(sync_experiments(paths))
             synced_paths.append((rig_name, data_path))
     
     return log, synced_paths
 
 
-def sync_paths(paths):
+def sync_experiments(paths):
+    """Given a list of paths to experiment site folders, synchronize all to the server
+    """
     log = []
     for site_dir in paths:
         try:
@@ -250,7 +190,7 @@ if __name__ == '__main__':
 
     else:
         # synchronize just the specified path(s)
-        log = sync_paths(paths)
+        log = sync_experiments(paths)
     
     errs = [change for site in log for change in site[1] if change[0] == 'error']
     print("\n----- DONE ------\n   %d errors" % len(errs))
