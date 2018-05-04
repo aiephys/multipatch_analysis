5fdae2b6d40e7743ddf2b3facf75533d799690c1
Corinne Teeter <corinnet@alleninstitute.org>
2018-05-04 14:00:17 -0700

    commit to transfer computers

--- analyses/test_psp_fitting.py
+++ analyses/test_psp_fitting.py
@@ -5,6 +5,7 @@ import numpy as np
 from pprint import pprint
 import json
 import neuroanalysis.data
+from multipatch_analysis.connection_detection import fit_psp
 
 test_data_dir='test_psp_fit' # directory containing test data
 
@@ -13,13 +14,13 @@ for file in test_data_files:
     test_dict=json.load(open(file)) # load test data
     avg_trace=neuroanalysis.data.Trace(data=np.array(test_dict['input']['data'])) # create Trace object
     psp_fits = fit_psp(avg_trace, 
-                       sign=save_dict['input']['amp_sign'], 
-                       yoffset=save_dict['input'][yoffset], 
-                       xoffset=save_dict['input'][xoffset], 
-                       amp=save_dict['input']['avg_amp'],
-                       method=save_dict['input']['method'], 
-                       stacked = save_dict['input']['stacled'], 
-                       rise_time_mult_factor=10., 
-                       fit_kws={'weights': weight})                        
+                       sign=test_dict['input']['amp_sign'], 
+                       yoffset=test_dict['input']['yoffset'], 
+                       xoffset=test_dict['input']['xoffset'], 
+                       amp=test_dict['input']['avg_amp'],
+                       method=test_dict['input']['method'], 
+                       stacked=test_dict['input']['stacked'], 
+                       rise_time_mult_factor=test_dict['input']['rise_time_mult_factor'], 
+                       fit_kws={'weight': test_dict['input']['weight']})                        
 
         
