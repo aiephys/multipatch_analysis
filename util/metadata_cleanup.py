27979daf0aa32691011cf1f36bba2a14c3a9b48c
ALLENINST\stephanies <stephanies@alleninstitute.org>
2018-05-10 14:33:14 -0700

    fixed carriage return and KeyError

--- util/metadata_cleanup.py
+++ util/metadata_cleanup.py
@@ -28,43 +28,49 @@ def solution_check(dh, expt_date, columns):
     date = expt_date.strftime('%#m/%#d/%Y')
     try:
         osm_entry = osm_dates[date]
-        recipe = osm_entry[columns['recipe_col']]
-        sheet_osm = osm_entry[columns['osm_col']]
-        if solution is None:
-            print("\tSet aCSF: %s\r" % recipe)
-            return
-
-        calcium = solution.split('m')[0]
-        if calcium in recipe:
-            print("\taCSF: Match!\r")
-        else:
-            print("\taCSF disagreement: %s != %s\r" % (solution, recipe))
-
-        if osm is None:
-            print("\tSet osmolarity: %s\r" % sheet_osm)
-            return
-
-        if sheet_osm == osm:
-            print("\tOsmolarity: Match!\r")
-        else:
-            print("\tOsmolarity disagreement: %s != %s\r" % (osm, sheet_osm))
     except KeyError:
         print("\tSolution date not in Multipatch_throughput sheet, check notes\r")
+        return
+
+    recipe = osm_entry[columns['recipe_col']]
+    sheet_osm = osm_entry[columns['osm_col']]
+    if solution is None:
+        print("\tSet aCSF: %s" % recipe)
+        return
+
+    calcium = solution.split('m')[0]
+    if calcium in recipe:
+        print("\taCSF: Match!")
+    else:
+        print("\taCSF disagreement: %s != %s" % (solution, recipe))
+
+    if osm is None:
+        print("\tSet osmolarity: %s" % sheet_osm)
+        return
+
+    if sheet_osm == osm:
+        print("\tOsmolarity: Match!")
+    else:
+        print("\tOsmolarity disagreement: %s != %s" % (osm, sheet_osm))
+        return
 
 
 def dissection_check(path, dh, sub_id, expt_date, columns):
     if sub_id is None:
-        print("\tNo animal_ID for %s\r" % path)
+        print("\tNo animal_ID for %s" % path)
         return
     dis_rec = diss_times.get(sub_id, None)
     if dis_rec is None:
-        print("\tNo tissue processing record\r")
+        print("\tNo tissue processing record")
         return
 
     date = dis_rec[columns['date_col']]
     sac = dis_rec[columns['sac_col']]
     time = dis_rec[columns['time_col']]
     sac_time = datetime.datetime.strptime(date + ' ' + sac, '%m/%d/%Y %I:%M %p')
+    if not time:
+        print("\tNo time record, sacrifice time was %s" % sac)
+        return
     if time[-2:] in ('AM', 'PM'):
         dis_time = datetime.datetime.strptime(date + ' ' + time, '%m/%d/%Y %I:%M %p')
     else:
@@ -73,7 +79,7 @@ def dissection_check(path, dh, sub_id, expt_date, columns):
             dis_time = dis_time + datetime.timedelta(seconds=12 * 3600)
 
     if (dis_time - sac_time).total_seconds() > 3600:
-        print("\tSac. / dissection times for are too far apart\r")
+        print("\tSac. / dissection times for are too far apart")
         return
     if dis_time <= sac_time:
         print("\tDissection before sac\r")
@@ -81,7 +87,7 @@ def dissection_check(path, dh, sub_id, expt_date, columns):
 
     if expt_date != dis_time.date():
         if (expt_date - dis_time.date()).days != 1:
-            print("\tExpt date %s does not match dissection date %s\r" % (expt_date, dis_time.date()))
+            print("\tExpt date %s does not match dissection date %s" % (expt_date, dis_time.date()))
             return
         dis_time_1 = "{d.year}-{d.month}-{d.day} {d.hour}:{d.minute:02d}".format(d=dis_time)
     else:
@@ -89,14 +95,14 @@ def dissection_check(path, dh, sub_id, expt_date, columns):
 
     dis_time_2 = dh.info().get('time_of_dissection', '')  # recorded by rig operator
     if dis_time_2 == '':
-        print("\tSet dissection time: %s\r" % dis_time_1)
+        print("\tSet dissection time: %s" % dis_time_1)
         return
     else:
         if dis_time_2 != dis_time_1:
-            print("\tDissection time disagreement:  %r != %r\r" % (dis_time_2, dis_time_1))
+            print("\tDissection time disagreement:  %r != %r" % (dis_time_2, dis_time_1))
             return
         else:
-            print("\tDissection Time: Match!\r")
+            print("\tDissection Time: Match!")
             return
 
 root = sys.argv[1]
@@ -109,8 +115,7 @@ for path in subs:
     dh = getDirHandle(path)
     sub_id = dh.info().get('animal_ID', None)
     expt_date = datetime.datetime.fromtimestamp(dh.info()['__timestamp__']).date()
-    print("Experiment Date: %s\r" % expt_date)
-    print("Animal ID: %s\r" % sub_id)
+    print("Experiment Date: %s\nAnimal ID: %s" % (expt_date, sub_id))
 
     dissection_check(path, dh, sub_id, expt_date, diss_columns)
     solution_check(dh, expt_date, osm_columns)
