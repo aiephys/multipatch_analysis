f503a974c9090324c39aacec4321736463b4cedb
Luke Campagnola <luke.campagnola@gmail.com>
2018-06-05 20:42:50 -0700

    fix submission tool pipette status list

--- multipatch_analysis/ui/ui.py
+++ multipatch_analysis/ui/ui.py
@@ -278,7 +278,7 @@ class PipetteParameter(pg.parametertree.parameterTypes.GroupParameter):
         self.ui = ui
         params = [
             {'name': 'channel', 'type': 'list', 'values': ui.list_channels()},
-            {'name': 'status', 'type': 'list', 'values': ['No seal', 'Low seal', 'GOhm seal', 'Technical failure']},
+            {'name': 'status', 'type': 'list', 'values': ['No seal', 'Low seal', 'GOhm seal', 'Technical failure', 'No attempt']},
             {'name': 'got data', 'type': 'bool'},
             {'name': 'internal', 'type': 'list', 'values': [''] + constants.INTERNAL_RECIPES},
             {'name': 'internal dye', 'type': 'list', 'values': [''] + constants.INTERNAL_DYES},
