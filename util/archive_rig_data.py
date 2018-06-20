ba9e0fdcde1c4f2b89f0ca5c76ad3fbb6af49f8b
Luke Campagnola <lukec@alleninstitute.org>
2018-06-20 16:54:20 -0700

    update rig archiving script to allow other source/dest

--- util/archive_rig_data.py
+++ util/archive_rig_data.py
@@ -1,11 +1,18 @@
 """
-Used to synchronize data on each rig from the primary storage drive (fast/small) to the archive storage drive (slow/large).
+Used to synchronize data on each rig from the primary storage drive (fast / small) to the archive storage drive (slow / large) and to the 
+backup drive (very slow / redundant).
 """
 import sys, argparse
 from multipatch_analysis import config, util
 
 
-def archive_rig_data(rig_name, test=False):
+def archive_rig_data(rig_name, test=False, source='primary', dest='archive'):
+    """Synchronize all rig data from one storage location to another.
+
+    The chosen *source* and *dest* storage locations must be defined in ``config.rig_data_paths[rig_name]``.
+
+    This copies/updates files but does not delete or rename any files in the archive.
+    """
     paths = config.rig_data_paths[rig_name]
     for path_set in paths:
         source = path_set['primary']
@@ -17,6 +24,8 @@ def archive_rig_data(rig_name, test=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--rig', type=str, default=config.rig_name, help="The name of the rig to archive (must be listed in config.rig_data_paths)")
+    parser.add_argument('--source', type=str, default='primary', help="The name of the source storage location ('primary', 'archive', or 'backup')")
+    parser.add_argument('--dest', type=str, default='archive', help="The name of the destination storage location ('primary', 'archive', or 'backup')")
     parser.add_argument('--test', action='store_true', default=False, help="Print actions to be taken, do not change any files")
     
     args = parser.parse_args(sys.argv[1:])
@@ -24,4 +33,4 @@ if __name__ == '__main__':
     if not args.rig[-1].isdigit():
         raise Exception('Rig name "%s" is invalid.')
 
-    archive_rig_data(rig_name=args.rig, test=args.test)
+    archive_rig_data(rig_name=args.rig, source=args.source, dest=args.dest, test=args.test)
