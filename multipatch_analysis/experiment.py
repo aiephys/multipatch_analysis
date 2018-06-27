f700b04c4f553109a75fa1ecc169cae84efe08cf
Luke Campagnola <lukec@alleninstitute.org>
2018-06-27 09:24:35 -0700

    temporary bandaid for cell qc cache

--- multipatch_analysis/experiment.py
+++ multipatch_analysis/experiment.py
@@ -333,7 +333,7 @@ class Experiment(object):
                     setattr(cell, k+'_qc', qc_pass)
             else:
                 # derive from NWB
-                cell.holding_qc, cell.access_qc, cell.spiking_qc = self._generate_cell_qc(pip_meta['ad_channel'])
+                cell.holding_qc, cell.access_qc, cell.spiking_qc = self._generate_cell_qc(pip_meta['ad_channel'], pip_id)
                 
         # load synapse/gap connections
         for cell in self.cells.values():
@@ -362,7 +362,7 @@ class Experiment(object):
                         raise ValueError("Postsynaptic cell ID %r is invalid" % post_id)
                     conn_list.append((cell.cell_id, post_id))
                 
-    def _generate_cell_qc(self, ad_chan):
+    def _generate_cell_qc(self, ad_chan, pipette_id):
         # tempporary qc used to decide how many connections were probed in an
         # experiment. will be replaced with per-pulse-response qc later.
         cache_file = os.path.join(os.path.dirname(config.configfile), 'cell_qc_cache.pkl')
@@ -375,44 +375,54 @@ class Experiment(object):
                 sys.excepthook(*sys.exc_info())
                 print("Failed to load cell qc cache (error above).")
         
+        # old key format
         cache_key = (self.nwb_file, ad_chan)
-        if cache_key not in cache:
-            nwb = self.data
-            holding_qc = False
-            access_qc = False
-            spiking_qc = False
-            try:
-                passed_holding = 0
-                for srec in nwb.contents:
-                    try:
-                        rec = srec[ad_chan]
-                    except KeyError:
-                        continue
-                    if rec.clamp_mode == 'vc':
-                        if rec.baseline_current is not None and abs(rec.baseline_current) < 800e-12:
-                            passed_holding += 1
-                    else:
-                        vm = rec.baseline_potential
-                        if vm > -75e-3 and vm < -50e-3:
-                            passed_holding += 1
-                    if passed_holding >= 5:
-                        break
+        if cache_key in cache:
+            return cache[cache_key]
+
+        cache_key = (self.timestamp, pipette_id)
+        if cache_key in cache:
+            return cache[cache_key]
+
+        # cache miss; generate new
+        print("Generating cell QC for", cache_key)
+        nwb = self.data
+        holding_qc = False
+        access_qc = False
+        spiking_qc = False
+        try:
+            passed_holding = 0
+            for srec in nwb.contents:
+                try:
+                    rec = srec[ad_chan]
+                except KeyError:
+                    continue
+                if rec.clamp_mode == 'vc':
+                    if rec.baseline_current is not None and abs(rec.baseline_current) < 800e-12:
+                        passed_holding += 1
+                else:
+                    vm = rec.baseline_potential
+                    if vm > -75e-3 and vm < -50e-3:
+                        passed_holding += 1
                 if passed_holding >= 5:
-                    holding_qc = True
-                    # need to fix these!
-                    access_qc = True
-                    spiking_qc = True
-            finally:
-                self.close_data()
-            cache[cache_key] = (holding_qc, access_qc, spiking_qc)
+                    break
+            if passed_holding >= 5:
+                holding_qc = True
+                # need to fix these!
+                access_qc = True
+                spiking_qc = True
+        finally:
+            self.close_data()
+        result = (holding_qc, access_qc, spiking_qc)
+        cache[cache_key] = result
+        
+        tmp_file = cache_file+'_tmp'
+        pickle.dump(cache, open(tmp_file, 'wb'))
+        if os.path.exists(cache_file):
+            os.remove(cache_file)
+        os.rename(tmp_file, cache_file)
             
-            tmp_file = cache_file+'_tmp'
-            pickle.dump(cache, open(tmp_file, 'wb'))
-            if os.path.exists(cache_file):
-                os.remove(cache_file)
-            os.rename(tmp_file, cache_file)
-            
-        return cache[cache_key]
+        return result
 
     def _load_old_format(self, entry):
         """Load experiment metadata from an old-style summary file
