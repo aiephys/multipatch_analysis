1646973aa4a01e7d30d73b14a2aefa633ff5658b
Luke Campagnola <lukec@alleninstitute.org>
2018-07-02 19:47:12 -0700

    connection strength cli cleanup

--- util/analyze_connection_strength.py
+++ util/analyze_connection_strength.py
@@ -6,13 +6,13 @@ from multipatch_analysis.connection_strength import (
 if __name__ == '__main__':
     import user
 
-    #tt = pg.debug.ThreadTrace()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rebuild', action='store_true', default=False)
-    parser.add_argument('--rebuild-connectivity', action='store_true', default=False, dest='rebuild_connectivity')
-    parser.add_argument('--local', action='store_true', default=False)
-    parser.add_argument('--workers', type=int, default=6)
+    parser.add_argument('--rebuild', action='store_true', default=False, "Rebuild connection strength and connectivity tables")
+    parser.add_argument('--rebuild-connectivity', action='store_true', default=False, dest='rebuild_connectivity', "Rebuild connectivity table, leaving connection strength table unaffected")
+    parser.add_argument('--local', action='store_true', default=False, "Disable concurrent processing to make debugging easier")
+    parser.add_argument('--workers', type=int, default=6, help="Set the number of concurrent processes during rebuild")
     parser.add_argument('--seed', type=int, default=-1, help="Random seed used to shuffle classifier training data")
+    parser.add_argument('--limit', type=int, default=0, help="Limit the number of experiments to process")
     
     args = parser.parse_args(sys.argv[1:])
     if args.rebuild:
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         connection_strength_tables.drop_tables()
         pulse_response_strength_tables.drop_tables()
         init_tables()
-        rebuild_strength(parallel=(not args.local), workers=args.workers)
+        rebuild_strength(limit=limit, parallel=(not args.local), workers=args.workers)
         rebuild_connectivity()
     elif args.rebuild_connectivity:
         print("drop tables..")
