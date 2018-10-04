6de8cdaa8f17c08a28807186d75ce9a9070b2448
Luke Campagnola <ausqualius@yahoo.com>
2018-10-04 15:04:25 -0700

    Add options to skip update steps

--- util/update_all.py
+++ util/update_all.py
@@ -8,6 +8,8 @@ debug problems more easily.
 
 import os, sys, time, argparse
 from datetime import datetime, timedelta
+from collections import OrderedDict
+
 
 def delay(hour=2):
     """Sleep until *hour*"""
@@ -20,30 +22,42 @@ def delay(hour=2):
     time.sleep(delay)
 
 
-def run(cmd, msg):
-    print("======================================================================================")
-    print("    " + msg)
-    print("======================================================================================")
-    os.system(cmd)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run all analysis pipeline stages to import / analyze new data on a schedule.")
     parser.add_argument('--now', default=False, action='store_true', help="Run once immediately before starting scheduled updates.")
-
+    parser.add_argument('--skip', default='', help="comma-separated list of stages to skip")
     args = parser.parse_args(sys.argv[1:])
 
     if not args.now:
         delay()
 
-    while True:
-        run('python util/sync_rigs_to_server.py', 'sync raw data')
-        run('python util/import_to_database.py', 'import to DB')
-        run('python util/update_morphology.py', 'update morphology')
-        run('python util/analyze_pulse_response_strength.py', 'pulse response strength')
-        run('python util/analyze_connection_strength.py', 'connection strength')
-        run('python util/database.py --vacuum', 'vacuum')
+    stages = OrderedDict([
+        ('sync',                    ('python util/sync_rigs_to_server.py', 'sync raw data')),
+        ('import',                  ('python util/import_to_database.py', 'import to DB')),
+        ('morphology',              ('python util/update_morphology.py', 'update morphology')),
+        ('pulse_response_strength', ('python util/analyze_pulse_response_strength.py', 'pulse response strength')),
+        ('connection_strength',     ('python util/analyze_connection_strength.py', 'connection strength')),
+        ('vacuum',                  ('python util/database.py --vacuum', 'vacuum')),
+    ])
 
+    skip = args.skip.split(',')
+    for name in skip:
+        if name not in stages:
+            print("Unknown stage %r. Options are: %r" % (name, list(stages.keys())))
+            sys.exit(-1)
+
+    while True:
+        for name, cmd in stages.items():
+            cmd, msg = cmd
+            print("======================================================================================")
+            print("    " + msg)
+            print("======================================================================================")
+            
+            if name in skip:
+                print("   [ skipping ]")
+                continue
+
+            os.system(cmd)
         delay()
 
 
