c63a4200fa0565b709e07ca2f8cc7a40827879e1
Luke Campagnola <ausqualius@yahoo.com>
2018-10-15 19:40:57 -0700

    New backup scripts

--- multipatch_analysis/util.py
+++ multipatch_analysis/util.py
@@ -1,30 +1,89 @@
 from __future__ import print_function
-import os, sys, time
+import os, sys, time, datetime, logging.handlers, re
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Set up default logging to stderr
+stderr_log_handler = logging.StreamHandler()
+stderr_log_handler.setLevel(logging.INFO)
+logger.addHandler(stderr_log_handler)
 
 
 def datetime_to_timestamp(d):
     return time.mktime(d.timetuple()) + d.microsecond * 1e-6
 
 
-def sync_dir(source_path, dest_path, test=False, make_dir=False):
+def sync_dir(source_path, dest_path, test=False, log_file=None, depth=0):
     """Safely duplicate a directory structure
-    """
-    assert os.path.isdir(source_path), 'Source path "%s" does not exist.' % source_path
-    if make_dir:
-        mkdir(dest_path)
-    assert os.path.isdir(dest_path), 'Destination path "%s" does not exist.' % dest_path
+    
+    All files/folders are recursively synchronized from source_path to dest_path.
+    Files in source_path that have a more recent modification time or a different file
+    size are copied, and the previous version is renamed with a timestamp suffix.
+    Likewise, files that exist in the destination but not the source are renamed.
 
-    for subpath, subdirs, files in os.walk(source_path):
-        rel = os.path.relpath(subpath, source_path)
-        dest_subpath = os.path.join(dest_path, rel)
-        if not os.path.exists(dest_subpath):
-            print('mkdir', dest_subpath)
-            if test is False:
-                os.mkdir(dest_subpath)
-        for fname in files:
-            src_file = os.path.join(subpath, fname)
-            dst_file = os.path.join(dest_subpath, fname)
-            action = sync_file(src_file, dst_file, test=test)
+    Parameters
+    ----------
+    source_path : str
+        Path to source files to be copied
+    dest_path : str
+        Path where files are copied to
+    test : bool
+        If True, then no changes are made to the destination
+    log_file : str
+        Path to a file for logging all chages made. Log files are rotated once per week.
+    """
+    log_handler = None
+    if log_file is not None and test is False:
+        log_file = os.path.abspath(log_file)
+        log_handler = logging.handlers.TimedRotatingFileHandler(log_file, when='W0', backupCount=50)
+        log_handler.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
+        logger.addHandler(log_handler)
+        log_handler.setLevel(logging.INFO)
+    
+    try:
+        source_path = os.path.abspath(source_path)
+        dest_path = os.path.abspath(dest_path)
+        if depth == 0:
+            logger.info("=== Begin directory sync %s => %s", source_path, dest_path)
+        
+        assert os.path.isdir(source_path), 'Source path "%s" does not exist.' % source_path
+        if test is False:
+            mkdir(dest_path, test=test)
+            assert os.path.isdir(dest_path), 'Destination path "%s" does not exist.' % dest_path
+
+        for child in os.listdir(source_path):
+            src_name = os.path.join(source_path, child)
+            dst_name = os.path.join(dest_path, child)
+            if os.path.isdir(src_name):
+                sync_dir(src_name, dst_name, test=test, depth=depth+1)
+            else:
+                sync_file(src_name, dst_name, test=test)
+
+        # check for deleted files
+        for child in os.listdir(dest_path):
+            src_name = os.path.join(source_path, child)
+            dst_name = os.path.join(dest_path, child)
+            
+            # log files are expected to exist only in destination 
+            if log_file is not None and dst_name.startswith(log_file):
+                continue
+            
+            # don't compare archived versions
+            if archived_filename(dst_name) is not None:
+                continue
+                
+            if not os.path.exists(src_name):
+                archive_file(dst_name, test=test)
+    
+    except BaseException as exc:
+        logger.error("Error during sync_dir(%s, %s): %s", source_path, dest_path, str(exc))
+    finally:
+        if depth == 0:
+            logger.info("=== Finished directory sync %s => %s", source_path, dest_path)
+        
+        if log_handler is not None:
+            logger.removeHandler(log_handler)
 
 
 def sync_file(src, dst, test=False):
@@ -36,12 +95,15 @@ def sync_file(src, dst, test=False):
         up_to_date = dst_stat.st_mtime >= src_stat.st_mtime and src_stat.st_size == dst_stat.st_size
         
         if up_to_date:
+            logger.debug("skip file: %s => %s", src, dst)
             return "skip"
         
         safe_copy(src, dst, test=test)
+        logger.info("update file: %s => %s", src, dst)
         return "update"
     else:
         safe_copy(src, dst, test=test)
+        logger.info("copy file: %s => %s", src, dst)
         return "copy"
 
 
@@ -53,36 +115,75 @@ def safe_copy(src, dst, test=False):
     tmp_dst = dst + '.partial'
     try:
         new_name = None
-        print("copy: %s => %s" % (src, dst))
         if os.path.exists(tmp_dst):
-            print("  remove stale .partial file")
             if test is False:
                 os.remove(tmp_dst)
         if test is False:
             chunk_copy(src, tmp_dst)
         if os.path.exists(dst):
-            # rename destination file to avoid overwriting
-            now = time.strftime('%Y-%m-%d_%H-%M-%S')
-            i = 0
-            while True:
-                new_name = '%s_%s_%d' % (dst, now, i)
-                if not os.path.exists(new_name):
-                    break
-                i += 1
-            print("  rename:", dst, new_name)
-            if test is False:
-                os.rename(dst, new_name)
-            print("  done")
+            new_name = archive_file(dst, test=test)
         if test is False:
             os.rename(tmp_dst, dst)
     except Exception:
         # Move dst file back if there was a problem during copy
         if test is False and new_name is not None and os.path.exists(new_name):
             os.rename(new_name, dst)
+        logger.error("error copying file: %s => %s", src, dst)
         raise
     finally:
+        # remove temporary file if needed
         if test is False and os.path.isfile(tmp_dst):
             os.remove(tmp_dst)
+
+
+archive_filename_format = '%Y-%m-%d_%H-%M-%S'
+archive_filename_regex = re.compile("(.*)_(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})_\d+")
+    
+
+def archive_file(filename, test=False):
+    """Rename a file so that it is suffixed with the current date and time.
+    
+    This is used to archive old files before a new version is copied in its place.
+    """
+    now = time.strftime(archive_filename_format)
+    i = 0
+    while True:
+        new_name = '%s_%s_%d' % (filename, now, i)
+        if not os.path.exists(new_name):
+            break
+        i += 1
+    logger.info("archive file: %s => %s", filename, new_name)
+    if test is False:
+        os.rename(filename, new_name)
+    
+    return new_name
+
+
+def archived_filename(filename):
+    """Given the name of a file that was created by archive_file(), return
+    the original file name, or None if the given file does not appear to be an archived copy.
+    """
+    m = archive_filename_regex.match(filename)
+    if m is None:
+        return None
+    return m.groups()[0]
+
+
+def archived_versions(filename):
+    """Return a list of (date, filename) pairs describing all archived versions of *filename*,
+    in ascending date order.
+    """
+    archives = []
+    path, filename = os.path.split(filename)
+    for child in os.listdir(path):
+        if not child.startswith(filename):
+            continue
+        m = archive_filename_regex.match(child)
+        if m is None:
+            continue
+        date = datetime.datetime.strptime(m.groups()[1], archive_filename_format)
+        archives.append((date, os.path.join(path, child)))
+    return sorted(archives)
     
 
 def chunk_copy(src, dst, chunk_size=100e6):
@@ -127,8 +228,12 @@ def chunk_copy(src, dst, chunk_size=100e6):
         raise
 
 
-def mkdir(path):
+def mkdir(path, test=False):
     if not os.path.isdir(path):
+        logger.info("mkdir: %s", path)
+        if test:
+            return
+    
         root, _ = os.path.split(path)
         if root != '':
             mkdir(root)
--- util/rig_backup.py
+++ util/rig_backup.py
@@ -1,13 +1,37 @@
-from __future__ import print_function
-import os, sys
-from multipatch_analysis.util import sync_dir
+"""
+For synchronizing data from archive storage on each rig to backup.
 
-args = sys.argv[1:]
-if '--test' in args:
-    test = True
-    args.remove('--test')
+Requires a structure in config.yml like:
+
+backup_paths:
+    rig1:
+        source: "V:\\"
+        dest: "L:\\rig1_backup"
+        exclude:
+            - "V:\\$Recycle Bin"
+    rig2:
+        source: "W:\\"
+        dest: "L:\\rig2_backup"
+
+"""
+from __future__ import print_function
+import os, sys, argparse
+from multipatch_analysis.util import sync_dir, logger
+from multipatch_analysis import config
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rigs', type=str, default="*", help="The name of the rig to back up (default is all rigs)")
+parser.add_argument('--test', action='store_true', default=False, help="Print actions to be taken, do not change any files")
+
+args = parser.parse_args(sys.argv[1:])
+
+if args.rigs == '*':
+    rigs = config.rig_backup_paths.keys()
 else:
-    test = False
-    
-source_path, dest_path = args
-sync_dir(source_path, dest_path, test=test)
+    rigs = args.rigs.split(',')
+
+for rig in rigs:
+    source_path, dest_path = config.rig_backup_paths[rig]
+    log_file = os.path.join(dest_path, 'backup.log')
+    sync_dir(source_path, dest_path, test=args.test, log_file=log_file)
