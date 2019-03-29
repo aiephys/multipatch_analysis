8674949ae4f85c3b0e0a9f53a135aaf8b83362d4
Luke Campagnola <lukec@alleninstitute.org>
2019-03-29 14:45:26 -0700

    db monitor stays open, runs in loop

--- util/db_monitor.py
+++ util/db_monitor.py
@@ -1,20 +1,21 @@
 """Simple tool for displaying DB connections, used for debugging performance issues
 """
-
+from __future__ import print_function, division
 import os, sys, subprocess, re
 from multipatch_analysis import config
 from sqlalchemy import create_engine
 
 
 engine = create_engine(config.synphys_db_host_rw + '/postgres?application_name=db_monitor')
-
+conn = engine.connect()
 
 
 def list_db_connections():
-    with engine.begin() as conn:
-        result = conn.execute("select client_addr, client_port, datname, query, state, usename, application_name, pid from pg_stat_activity;")
-        connections = result.fetchall()
-    return connections
+    tr = conn.begin()
+    query = conn.execute("select client_addr, client_port, datname, query, state, usename, application_name, pid from pg_stat_activity;")
+    result = query.fetchall()
+    tr.rollback()
+    return result
 
 
 _known_hostnames = {}
@@ -66,4 +67,43 @@ def check():
                 print("          {:15s} {:15s} {:45s} {:6d} {:10s} {:s}   ".format(con.usename, con.datname, app[:45], con.pid, state, query))
     
 
-check()
+class ScreenPrint(object):
+    """Simple interface for printing full-screen text
+    """
+    def __init__(self):
+        global print
+        self._print = print
+        print = self.print_line
+        
+        self.current_row = 0
+        self.rows = 100
+        self.columns = 100
+
+    def reset(self):
+        self.current_row = 0
+        self._print("\033[0;0f")
+        self.rows, self.columns = list(map(int, os.popen('stty size', 'r').read().split()))
+
+    def print_line(self, msg):
+        while len(msg) > self.columns:
+            line, msg = msg[:self.columns], msg[self.columns:]
+            self._print(line)
+            self.current_row += 1
+        self._print(("{:%ds}" % self.columns).format(msg))
+        self.current_row += 1
+        
+    def clear_to_bottom(self):
+        for i in range(self.current_row, self.rows-1):
+            self.print_line("")
+
+
+if __name__ == '__main__':
+    import time
+    
+    sp = ScreenPrint()
+    
+    while True:
+        sp.reset()
+        check()
+        sp.clear_to_bottom()        
+        time.sleep(5)
