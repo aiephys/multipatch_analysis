610026a1b81565628022c7be74c68db157a6a285
Corinne <corinnet@alleninstitute.org>
2019-04-09 13:02:09 -0700

    adding known errors

--- multipatch_analysis/pipeline/first_pulse_fit.py
+++ multipatch_analysis/pipeline/first_pulse_fit.py
@@ -7,6 +7,8 @@ from .. import config
 from .pipeline_module import DatabasePipelineModule
 from .connection_strength import ConnectionStrengthPipelineModule
 from ..fit_average_first_pulse import fit_first_pulses
+import traceback
+import sys
 
 
 class FirstPulseFitPipelineModule(DatabasePipelineModule):
@@ -18,13 +20,31 @@ class FirstPulseFitPipelineModule(DatabasePipelineModule):
     
     @classmethod
     def create_db_entries(cls, expt_id, session):
-    
+   
         expt = db.experiment_from_timestamp(expt_id, session=session)
-        
+       
+        fails = []
+        errors = ""
         for (pre_cell_id, post_cell_id), pair in expt.pairs.items():
-            result = fit_first_pulses(pair, pre_cell_id, post_cell_id)
-            afpf = db.AvgFirstPulseFit(pair=pair, **result)
-            session.add(afpf)
+            try:
+                result = fit_first_pulses(pair)
+                if result['error'] is not None:
+                    # known error occurred; we consider this a successful run
+                    errors += "(%d->%d) %s\n\n" % (pre_cell_id, post_cell_id, result['error'])
+                else:
+                    result.pop('error')
+                    afpf = db.AvgFirstPulseFit(pair=pair, **result)
+                    session.add(afpf)
+            except:
+                # unhandled exception occurred; we consider this an unsuccessful run
+                exc = traceback.format_exception(*sys.exc_info())
+                errors += "(%d->%d) Exception\n%s" % (pre_cell_id, post_cell_id, exc)
+                fails.append((pre_cell_id, post_cell_id))
+               
+        if len(fails) > 0:
+            raise Exception("Exceptions occurred processing pairs: %s\n\n%s" % (fails, errors))
+        else:
+            return errors
         
     @classmethod
     def job_records(cls, job_ids, session):
