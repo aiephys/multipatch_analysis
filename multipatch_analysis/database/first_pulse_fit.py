ec7efdce13069e2d7752fd52e95848f58f9f29cd
Corinne <corinnet@alleninstitute.org>
2019-04-10 18:49:15 -0700

    added single pulse fit schema (not yest tested)

--- multipatch_analysis/database/first_pulse_fit.py
+++ multipatch_analysis/database/first_pulse_fit.py
@@ -3,17 +3,18 @@ from .database import make_table, TableGroup
 from .experiment import Pair
 
 
-__all__ = ['first_pulse_fit_tables', 'AvgFirstPulseFit']
+__all__ = ['first_pulse_fit_tables', 'AvgFirstPulseFit', 'SingleFirstPulseFit']
 
 
 AvgFirstPulseFit = make_table(
     name='avg_first_pulse_fit',
     comment="""Contains results of psp_fit on spike aligned, average first pulse PSP for each
-            connection that passed qc in current clamp. During the fit the latency is forced
-            to be the value +/-.5 ms found via fitting all of the pulses (available in the 
+            connection that passed qc in current clamp. The latency is forced
+            to be within +/-.5 ms to the found fitting all of the pulses in the train (available in the 
             connection_strength.ic_fit_xoffset). The heavily weighted section (meant to 
             place more importance of the wave form during the rise time) is shifted to 
             begin at the latency. Created via fit_average_first_pulse.py. 
+
             All units in SI.""",
     columns=[
         ('pair_id', 'pair.id', 'The ID of the entry in the pair table to which these results apply', {'index': True}),
@@ -48,7 +49,43 @@ AvgFirstPulseFit = make_table(
     ]
 )
 
+SingleFirstPulseFit = make_table(
+    name='single_first_pulse_fit',
+    comment="""Contains results of psp_fit on individual first pulses. The latency is forced
+            to the value found via fitting the average of the first pulses (available in the
+            avg_first_pulse_fit table). The heavily weighted section (meant to 
+            place more importance of the wave form during the rise time) is shifted to 
+            begin at the latency. Created via fit_single_first_pulse.py. 
+            All units in SI.""",
+    columns=[
+        ('pulse_response_id', 'pulse_response.id', 'The ID of the entry in the pulse_response table', {'index': True}),
+
+        # current clamp
+        ('ic_amp', 'float', 'fit amplitude of current clamp first pulses'),
+        ('ic_rise_time', 'float', 'fit rise time of psp of current clamp data'),
+        ('ic_decay_tau', 'float', 'fit decay of psp of current clamp data'),
+        ('ic_avg_psp_data', 'array', 'array of the data voltage waveform used in fitting; starts 10 ms before pre-synaptic spike'),
+        ('ic_avg_psp_fit', 'array', 'fit array of the best fit voltage waveform starting 10 ms before pre-synaptic spike'),
+        ('ic_dt', 'float', 'time step of *avg_psp* array from current clamp data'),
+        ('ic_nrmse', 'float', 'error of fit of current clamp fit'),
+
+        # voltage clamp
+        ('vc_amp', 'float', 'fit amplitude of voltage clamp first pulses'),
+        ('vc_rise_time', 'float', 'fit rise time of psp measured in voltage clamp'),
+        ('vc_decay_tau', 'float', 'fit decay of psp measured in voltage clamp'),
+        ('vc_avg_psp_data', 'array', 'array of the data current waveform used in fitting; starts 10 ms before pre-synaptic spike'),            
+        ('vc_avg_psp_fit', 'array', 'fit array of the best fit current waveform starting 10 ms before pre-synaptic spike'),
+        ('vc_dt', 'float', 'time step of *avg_psp* array from voltage clamp data'),
+        ('vc_nrmse', 'float', 'error of fit of voltage clamp fit'),
+    ]
+)
+
+
+
 Pair.avg_first_pulse_fit = relationship(AvgFirstPulseFit, back_populates="pair", cascade="delete", single_parent=True, uselist=False)
 AvgFirstPulseFit.pair = relationship(Pair, back_populates="avg_first_pulse_fit", single_parent=True)
 
-first_pulse_fit_tables = TableGroup([AvgFirstPulseFit])
+PulseResponse.single_first_pulse_fit = relationship(SingleFirstPulseFit, back_populates="pulse_response", cascade="delete", single_parent=True, uselist=False)
+SingleFirstPulseFit.pulse_response = relationship(PulseResponse, back_populates="single_first_pulse_fit", single_parent=True)
+
+first_pulse_fit_tables = TableGroup([AvgFirstPulseFit, SingleFirstPulseFit])
