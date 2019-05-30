1b144ccb1ce58e5754ab9efc4c179791ec512386
Luke Campagnola <lukec@alleninstitute.org>
2019-05-30 14:16:05 -0700

    Add genotype parts

--- multipatch_analysis/genotypes.py
+++ multipatch_analysis/genotypes.py
@@ -69,8 +69,10 @@ DRIVER_LINES = {                  # dependencies     products
     'Slc17a6-IRES2-FlpO':         [(['slc17a6'],      ['flp'])],
     'Slc17a8-iCre':               [(['slc17a8'],      ['cre'])],
     'Ctgf-T2A-dgCre':             [(['ctgf'],         ['cre'])],
+    'Ndnf-IRES2-dgCre':           [(['ndnf'],         ['cre'])],
+    'Slc32a1-IRES2-FlpO':         [(['slc32a1'],      ['flp'])],
     'Fam84b-FlpO':                [(['fam84b'],       ['flp'])],
-    'rAAV-mDlx-GFP':              [(['mDlx'],         ['GFP'])],
+    'rAAV-mDlx-GFP':              [(['mDlx'],         ['GFP'])],    
     'rAAV-Dlx2.0-SYFP2':          [(['3xcorehI56i'],  ['YFP'])],  # pan-GABA
     'rAAV-eHGT_078m-minBglobin-SYFP2-WPRE3-BGHpA': [
                                    (['eHGT_078m'], ['YFP'])],  # pan-Glu
