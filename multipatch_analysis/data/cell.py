c30a12a5843cb74e59ef729492fe5fdfdfc5242b
Luke Campagnola <lukec@alleninstitute.org>
2019-08-19 23:36:05 -0700

    minor fix

--- multipatch_analysis/data/cell.py
+++ multipatch_analysis/data/cell.py
@@ -151,4 +151,4 @@ class Cell(object):
         return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)**0.5
 
     def __repr__(self):
-        return "<Cell %s:%d>" % (self.expt.source_id, self.cell_id)
+        return "<Cell %s:%s>" % (self.expt.source_id, self.cell_id)
