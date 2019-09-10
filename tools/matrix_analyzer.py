94df4cdca0e8278dd1ecb3a2de8b623f4c2794aa
Megan Kratz <meganbkratz@gmail.com>
2019-09-10 13:30:04 -0600

    add --debug command line option

--- tools/matrix_analyzer.py
+++ tools/matrix_analyzer.py
@@ -13,10 +13,14 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, default='external')
+    parser.add_argument('--debug', action='store_true', default=False, help="Raise a pyqtgraph debug console.")
     args = parser.parse_args(sys.argv[1:])
     analyzer_mode = args.mode
 
     session = db.session()
+
+    if args.debug:
+        pg.dbg()
     
     # Define cell classes
     cell_class_groups = OrderedDict([
