0eaf718c6a8770a22375f008c2049a9e5811c8a5
Luke Campagnola <lukec@alleninstitute.org>
2019-09-13 03:32:02 -0700

    new stp metrics

--- multipatch_analysis/database/schema/dynamics.py
+++ multipatch_analysis/database/schema/dynamics.py
@@ -11,20 +11,18 @@ Dynamics = make_table(
     comment="Describes short term dynamics of synaptic connections.",
     columns=[
         ('pair_id', 'pair.id', 'The ID of the cell pair described by each record', {'index': True, 'unique': True}),
-        ('pulse_ratio_8_1_50hz', 'float', '8:1 pulse ratio for 50Hz induction', {'index': True}),
-        ('pulse_ratio_2_1_50hz', 'float', '2:1 pulse ratio for 50Hz induction', {'index': True}),
-        ('pulse_ratio_5_1_50hz', 'float', '5:1 pulse ratio for 50Hz induction', {'index': True}),
-        ('pulse_ratio_9_1_125ms', 'float', '9:1 pulse ratio for 50Hz induction at 125ms delay', {'index': True}),
-        ('pulse_ratio_9_1_250ms', 'float', '9:1 pulse ratio for 50Hz induction at 250ms delay', {'index': True}),
-        ('pulse_ratio_9_1_500ms', 'float', '9:1 pulse ratio for 50Hz induction at 500ms delay', {'index': True}),
-        ('pulse_ratio_9_1_1000ms', 'float', '9:1 pulse ratio for 50Hz induction at 1000ms delay', {'index': True}),
-        ('pulse_ratio_9_1_2000ms', 'float', '9:1 pulse ratio for 50Hz induction at 2000ms delay', {'index': True}),
-        ('pulse_ratio_9_1_4000ms', 'float', '9:1 pulse ratio for 50Hz induction at 4000ms delay', {'index': True}),
-        ('pulse_ratio_8_1_10hz', 'float', '8:1 pulse ratio for 10Hz induction', {'index': True}),
-        ('pulse_ratio_8_1_20hz', 'float', '8:1 pulse ratio for 20Hz induction', {'index': True}),
-        ('pulse_ratio_8_1_100hz', 'float', '8:1 pulse ratio for 100Hz induction', {'index': True}),
-        ('pulse_ratio_8_1_200hz', 'float', '8:1 pulse ratio for 200Hz induction', {'index': True}),
-
+        ('paired_pulse_ratio_50hz', 'float', 'The mean ratio of 2nd / 1st pulse amplitudes for 50Hz pulse trains.', {'index': True}),
+        ('stp_initial_50hz', 'float', 'The mean relative change from 1st to 2nd pulse for 50Hz pulse trains', {'index': True}),
+        ('stp_initial_50hz_n', 'float', 'Number of samples represented in stp_initial_50Hz', {'index': True}),
+        ('stp_initial_50hz_std', 'float', 'Standard deviation of samples represented in stp_initial_50Hz', {'index': True}),
+        ('stp_induction_50hz', 'float', 'The mean relative change from 1st to 5th-8th pulses for 50Hz pulse trains', {'index': True}),
+        ('stp_induction_50hz_n', 'float', 'Number of samples represented in stp_induction_50Hz', {'index': True}),
+        ('stp_induction_50hz_std', 'float', 'Standard deviation of samples represented in stp_induction_50Hz', {'index': True}),
+        ('stp_recovery_250ms', 'float', 'The mean relative change from 1st-4th to 9th-12th pulses for pulse trains with a 250 ms recovery period', {'index': True}),
+        ('stp_recovery_250ms_n', 'float', 'Number of samples represented in stp_recovery_250ms', {'index': True}),
+        ('stp_recovery_250ms_std', 'float', 'Standard deviation of samples represented in stp_recovery_250ms', {'index': True}),
+        ('pulse_amp_90th_percentile', 'float', 'The 90th-percentile largest pulse amplitude, used to normalize change values in this table', {}),
+        ('noise_amp_90th_percentile', 'float', 'The 90th-percentile largest amplitude measured from background noise, used for comparison to pulse_amp_90th_percentile', {}),
     ]
 )
 
--- multipatch_analysis/pipeline/multipatch/dynamics.py
+++ multipatch_analysis/pipeline/multipatch/dynamics.py
@@ -9,9 +9,11 @@ import os
 import numpy as np
 from collections import OrderedDict
 from ...util import timestamp_to_datetime
+from ...dynamics import generate_pair_dynamics
 from ..pipeline_module import DatabasePipelineModule
 from .pulse_response import PulseResponsePipelineModule
 from .synapse_prediction import SynapsePredictionPipelineModule
+
 
 
 class DynamicsPipelineModule(DatabasePipelineModule):
@@ -30,83 +32,11 @@ class DynamicsPipelineModule(DatabasePipelineModule):
         # Load experiment from DB
         expt = db.experiment_from_timestamp(job_id, session=session)
         for pair in expt.pairs.values():
-            if pair.has_synapse is False:
+            if pair.has_synapse is not True:
                 continue
-                
-            recs = session.query(db.Recording).join(db.PulseResponse).join(db.Pair).filter(db.Pair.id==pair.id).all()
-            pulse_amps = {}
-            for rec in recs:
-                q = session.query(db.PulseResponseFit, db.PulseResponse, db.StimPulse.pulse_number, db.MultiPatchProbe.induction_frequency, db.MultiPatchProbe.recovery_delay)
-                q = q.join(db.PulseResponse, db.PulseResponseFit.pulse_response)
-                q = q.join(db.StimPulse, db.PulseResponse.stim_pulse)
-                q = q.join(db.PatchClampRecording, db.PatchClampRecording.recording_id==db.PulseResponse.recording_id)
-                q = q.join(db.MultiPatchProbe)
-                q = q.filter(db.PulseResponse.pair_id==pair.id).filter(db.PatchClampRecording.clamp_mode=='ic').filter(db.PulseResponse.recording_id==rec.id)
-                results = q.all()
-                
-                if len(results) != 12:
-                    continue
-
-                sign = pair.synapse.synapse_type
-                qc = sign+'_qc_pass'
-                amp_field = 'fit_amp'
-                # make sure all 12 pulses pass qc before moving on
-                qc_check = [getattr(r.pulse_response, qc) and getattr(r.pulse_response_fit, amp_field) is not None for r in results]
-                if all(qc_check) is False:
-                    continue
-
-                ind_freq = results[0].induction_frequency
-                rec_delay = results[0].recovery_delay
-                # round measured recovery delay to known delays defined in experiments, if the rounded value differs by more than 5ms ignore
-                if rec_delay is None:
-                    rec_delay_rounded = None
-                else:
-                    rec_delay_rounded = min(delays, key=lambda x:abs(x-rec_delay*1000))
-                    delay_dist = abs(rec_delay_rounded - rec_delay*1000)
-                    pulse_amps.setdefault(rec_delay_rounded, {})
-                    if delay_dist > 5:
-                        rec_delay_rounded = None
-                        
-                pulse_amps.setdefault(ind_freq, {})
-                for result in results:
-                    pulse_number = result.pulse_number
-                    if ind_freq == 50 and rec_delay_rounded is not None:
-                        pulse_amps[rec_delay_rounded].setdefault(pulse_number, [])
-                        pulse_amps[rec_delay_rounded][pulse_number].append(getattr(result.pulse_response_fit, amp_field))
-                    pulse_amps[ind_freq].setdefault(pulse_number, [])
-                    pulse_amps[ind_freq][pulse_number].append(getattr(result.pulse_response_fit, amp_field))
-            if any(pulse_amps):
-                ## pulse amps is a nested dictionary with floats for keys {induction_frequency (ex 50): {pulse_number (ex 1): [list of pulse amps], 2: [pulse_amps]...}}
-                pulse_ratio_8_1_50hz = np.nanmean(pulse_amps.get(50, {}).get(8, np.nan)) / np.nanmean(pulse_amps.get(50, {}).get(1, np.nan))
-                pulse_ratio_9_1_125ms = np.nanmean(pulse_amps.get(125, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(125, {}).get(1, np.nan))
-                pulse_ratio_9_1_250ms = np.nanmean(pulse_amps.get(250, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(250, {}).get(1, np.nan))
-                pulse_ratio_9_1_500ms = np.nanmean(pulse_amps.get(500, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(500, {}).get(1, np.nan))
-                pulse_ratio_9_1_1000ms = np.nanmean(pulse_amps.get(1000, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(1000, {}).get(1, np.nan))
-                pulse_ratio_9_1_2000ms = np.nanmean(pulse_amps.get(2000, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(2000, {}).get(1, np.nan))
-                pulse_ratio_9_1_4000ms = np.nanmean(pulse_amps.get(4000, {}).get(9, np.nan)) / np.nanmean(pulse_amps.get(4000, {}).get(1, np.nan))
-                pulse_ratio_2_1_50hz = np.nanmean(pulse_amps.get(50, {}).get(2, np.nan)) / np.nanmean(pulse_amps.get(50, {}).get(1, np.nan))
-                pulse_ratio_5_1_50hz = np.nanmean(pulse_amps.get(50, {}).get(5, np.nan)) / np.nanmean(pulse_amps.get(50, {}).get(1, np.nan)) 
-                pulse_ratio_8_1_10hz = np.nanmean(pulse_amps.get(10, {}).get(8, np.nan)) / np.nanmean(pulse_amps.get(10, {}).get(1, np.nan))  
-                pulse_ratio_8_1_20hz = np.nanmean(pulse_amps.get(20, {}).get(8, np.nan)) / np.nanmean(pulse_amps.get(20, {}).get(1, np.nan))
-                pulse_ratio_8_1_100hz = np.nanmean(pulse_amps.get(100, {}).get(8, np.nan)) / np.nanmean(pulse_amps.get(100, {}).get(1, np.nan))   
-                pulse_ratio_8_1_200hz = np.nanmean(pulse_amps.get(200, {}).get(8, np.nan)) / np.nanmean(pulse_amps.get(200, {}).get(1, np.nan))   
-
-                # Write new record to DB
-                dynamics = db.Dynamics(pair_id=pair.id, 
-                    pulse_ratio_2_1_50hz=pulse_ratio_2_1_50hz, 
-                    pulse_ratio_8_1_50hz=pulse_ratio_8_1_50hz, 
-                    pulse_ratio_5_1_50hz=pulse_ratio_5_1_50hz,
-                    pulse_ratio_9_1_125ms=pulse_ratio_9_1_125ms,
-                    pulse_ratio_9_1_250ms=pulse_ratio_9_1_250ms,
-                    pulse_ratio_9_1_500ms=pulse_ratio_9_1_500ms,
-                    pulse_ratio_9_1_1000ms=pulse_ratio_9_1_1000ms,
-                    pulse_ratio_9_1_2000ms=pulse_ratio_9_1_2000ms,
-                    pulse_ratio_9_1_4000ms=pulse_ratio_9_1_4000ms,
-                    pulse_ratio_8_1_10hz=pulse_ratio_8_1_10hz,
-                    pulse_ratio_8_1_20hz=pulse_ratio_8_1_20hz,
-                    pulse_ratio_8_1_100hz=pulse_ratio_8_1_100hz,
-                    pulse_ratio_8_1_200hz=pulse_ratio_8_1_200hz)
-                session.add(dynamics)
+            
+            dynamics = generate_pair_dynamics(pair, db, session)
+            session.add(dynamics)
         
     def job_records(self, job_ids, session):
         """Return a list of records associated with a list of job IDs.
