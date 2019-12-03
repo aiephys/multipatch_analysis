83d91e16709d03f8baeade8ce61bc55dc7fbfe1d
ALLENINST\stephanies <stephanies@alleninstitute.org>
2019-12-03 14:02:02 -0800

    mMPATCH_191203_05_06

--- aisynphys/ui/acq4_modules.py
+++ aisynphys/ui/acq4_modules.py
@@ -400,7 +400,7 @@ class PatchSeqMetadata(MetadataField):
             def setWidgets(self, columns):
                 self.widgets = {
                 'Seal': pg.ComboBox(items=['','GS','LS','NS','TF','NA']),
-                'Reporter': pg.ComboBox(items=['','red', 'green', 'yellow' '-', 'NA']),
+                'Reporter': pg.ComboBox(items=['','red', 'green', 'yellow', '-', 'NA']),
                 'Nucleus': pg.ComboBox(items=['','+', '-']),
                 'End Seal': pg.QtGui.QCheckBox(''),
                 'Tube ID': pg.QtGui.QLineEdit(),
