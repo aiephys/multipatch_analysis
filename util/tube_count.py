0376b4682d111c778343c0f859f72fa7bf5d146f
ALLENINST\stephanies <stephanies@alleninstitute.org>
2020-02-03 09:44:07 -0800

    tube_count update

--- util/tube_count.py
+++ util/tube_count.py
@@ -4,19 +4,10 @@ from aisynphys.database import default_db as db
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--month', nargs=1, type=int)
-    parser.add_argument('--year', nargs=1, type=int)
+    args = sys.argv[1:]
 
-    args = parser.parse_args(sys.argv[1:])
-
-    month = args.month
-    year = args.year
-
-    if year is None:
-        year = 2020
-    else:
-        year = year[0]
+    start = datetime.strptime(args[0], "%y%m%d")
+    stop = datetime.strptime(args[1], "%y%m%d")
 
     tubes = db.query(db.PatchSeq.tube_id).all()
     n_tubes = 0
@@ -26,11 +17,7 @@ if __name__ == '__main__':
         if name_check is not None:
             date=name_check.group('date')
             date=datetime.strptime(date, "%y%m%d")
-            if month is not None:
-                if date.month==month[0] and date.year==year:
-                    n_tubes+=1
-            else:
-                if date.year == year:
-                    n_tubes+=1
+            if date >= start and date <= stop:
+                n_tubes+=1
 
-    print("Number of tubes for %s %s: %d" % (month, year, n_tubes))
+    print("Number of tubes from %s - %s: %d" % (start.date(), stop.date(), n_tubes))
