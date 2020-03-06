fc1eba146cfa411a55c457e46e9523b5f239549e
Luke Campagnola <lukec@alleninstitute.org>
2020-03-06 11:32:35 -0800

    simplify nwb viewer; remove acq4 dependency

--- tools/mies_nwb_viewer.py
+++ tools/mies_nwb_viewer.py
@@ -1,7 +1,6 @@
 import os, sys
-import acq4
-from acq4.pyqtgraph.Qt import QtCore, QtGui
-import acq4.pyqtgraph.console
+import pyqtgraph as pg
+import pyqtgraph.console
 
 from aisynphys.ui.multipatch_nwb_viewer import MultipatchNwbViewer
 
@@ -14,17 +13,13 @@ if __name__ == '__main__':
     sweeps : A list of the currently selected sweeps
     view : The MiesNwbViewer instance
     nwb : The currently loaded NWB file
-    man : ACQ4 manager
-    man.currentFile : currently selected file in ACQ4 DataManager module
 
     """
-    console = acq4.pyqtgraph.console.ConsoleWidget(text=message)
+    pg.mkQApp()
+    
+    console = pg.console.ConsoleWidget(text=message)
     console.catchNextException()
     console.show()
-
-    # start up ACQ4 data manager
-    m = acq4.Manager.Manager(argv=['-D', '-n', '-m', 'Data Manager'])
-    dm = m.getModule('Data Manager')
 
     # create NWB viewer
     v = MultipatchNwbViewer()
@@ -39,17 +34,8 @@ if __name__ == '__main__':
         nwb = v.load_nwb(filename)
         console.localNamespace['nwb'] = nwb
 
-    # add a button to load nwb from file selected in data manager
-    def load_from_dm():
-        with acq4.pyqtgraph.BusyCursor():
-            load_nwb(m.currentFile.name())
-
-    btn = acq4.pyqtgraph.Qt.QtGui.QPushButton('load from data manager')
-    v.vsplit.insertWidget(0, btn)
-    btn.clicked.connect(load_from_dm)
-
     # make a few variables available from the console
-    console.localNamespace.update({'man': m, 'view': v, 'nwb': None, 'sweeps': [], 'analyzer': None})
+    console.localNamespace.update({'view': v, 'nwb': None, 'sweeps': [], 'analyzer': None})
 
     # make selected sweeps / analyzer available as well
     def update_namespace():
@@ -57,14 +43,6 @@ if __name__ == '__main__':
 
     v.analyzer_changed.connect(update_namespace)
     v.explorer.selection_changed.connect(update_namespace)
-
-    # Set up code reloading shortcut
-    def reload_all():
-        acq4.pyqtgraph.reload.reloadAll(verbose=True)
-
-    reload_shortcut = QtGui.QShortcut(QtGui.QKeySequence('Ctrl+r'), v)
-    reload_shortcut.setContext(QtCore.Qt.ApplicationShortcut)
-    reload_shortcut.activated.connect(reload_all)
 
     # load file or set base directory from argv
     for arg in sys.argv[1:]:
@@ -77,11 +55,9 @@ if __name__ == '__main__':
                 sys.exit(-1)
             arg = os.path.join(expt.nwb_file)
 
-        if os.path.isdir(arg):
-            m.setBaseDir(arg)
-        elif os.path.isfile(arg):
+        if os.path.isfile(arg):
             load_nwb(arg)
 
     # start Qt event loop if this is not an interactive python session
     if sys.flags.interactive == 0:
-        acq4.pyqtgraph.QtGui.QApplication.exec_()
+        pg.QtGui.QApplication.exec_()
