9f6df93193f7b2de65f999c8b5bc6d84c36327c2
Luke Campagnola <luke.campagnola@gmail.com>
2020-07-13 10:51:44 -0700

    remove commented code

--- aisynphys/data/data.py
+++ aisynphys/data/data.py
@@ -252,39 +252,6 @@ class MultiPatchSyncRecAnalyzer(Analyzer):
             artifacts = True
         return artifacts
 
-### Moved to neuroanalysis.analyzers.baseline
-# class BaselineDistributor(Analyzer):
-#     """Used to find baseline regions in a trace and distribute them on request.
-#     """
-#     def __init__(self, rec):
-#         self._attach(rec)
-#         self.rec = rec
-#         self.baselines = [list(r) for r in rec.baseline_regions]
-
-#     def get_baseline_chunk(self, duration=20e-3):
-#         """Return the (start, stop) indices of a chunk of unused baseline with the
-#         given duration.
-#         """
-#         for i, baseline_rgn in enumerate(self.baselines):
-#             rgn_start, rgn_stop = baseline_rgn
-#             if rgn_stop - rgn_start < duration:
-#                 continue
-#             chunk_stop = rgn_start + duration
-#             baseline_rgn[0] = chunk_stop
-#             return rgn_start, chunk_stop
-        
-#         # coundn't find any baseline data of the requested length
-#         return None
-
-#     def baseline_chunks(self, duration=20e-3):
-#         """Iterator yielding (start, stop) indices of baseline chunks.
-#         """
-#         while True:
-#             chunk = self.get_baseline_chunk(duration)
-#             if chunk is None:
-#                 break
-#             yield chunk
-
 
 class PulseResponse(object):
     """Represents a chunk of postsynaptic recording taken during a presynaptic pulse stimulus.
