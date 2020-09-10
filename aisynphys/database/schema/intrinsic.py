fbb29d8ee30bf91e79c0c85d68343219adf7d412
Tom Chartrand <tom.chartrand@alleninstitute.org>
2020-09-09 17:54:47 -0700

    add chirp features

--- aisynphys/database/schema/intrinsic.py
+++ aisynphys/database/schema/intrinsic.py
@@ -8,14 +8,13 @@ __all__ = ['Intrinsic']
 
 Intrinsic = make_table(
     name='intrinsic',
-    comment= "Describes the intrinsic properties of cells using 1 sec long current steps.",
+    comment= "Describes the intrinsic properties of cells using 1 sec long current steps and chirps.",
     columns=[
         ('cell_id', 'cell.id', 'The ID of the entry in the cell table to which these results apply', {'index': True}),
         ('rheobase', 'float', 'Current at rheobase', {'index': True}),
         ('fi_slope', 'float', 'Slope of the current-spiking relationship', {'index': True}),
         ('input_resistance', 'float', 'Input resistance of the cell', {'index': True}),
         ('sag', 'float', 'Hyperpolarizing sag measured from ~ -100mV current injection', {'index': True}),
-        ('avg_firing_rate', 'float', 'Average firing rate of cell', {'index': True}),
         ('adaptation_index', 'float', 'Average adaptation index', {'index': True}),
         ('upstroke_downstroke_ratio', 'float', 'The upstroke-downstroke ratio of the first spike', {'index': True}),
         ('width', 'float', 'Spike width', {'index': True}),
@@ -24,6 +23,9 @@ Intrinsic = make_table(
         ('peak_v', 'float', 'Spike peak voltage', {'index': True}),
         ('threshold_v', 'float', 'Spike threshold voltage', {'index': True}),
         ('fast_trough_v', 'float', 'AHP / fast trough voltage', {'index': True}),
+        ('chirp_peak_freq', 'float', 'Frequency at which the chirp response peaks', {'index': True}),
+        ('chirp_3db_freq', 'float', 'Frequency at which the chirp response amplitude is 3 dB below the peak.', {'index': True}),
+        ('chirp_peak_ratio', 'float', 'Ratio of chirp resonance peak amplitude to low-frequency response amplitude', {'index': True}),
         
     ]
 )
--- aisynphys/pipeline/multipatch/intrinsic.py
+++ aisynphys/pipeline/multipatch/intrinsic.py
@@ -9,11 +9,12 @@ import traceback, sys
 import numpy as np   
 from ipfx.data_set_features import extractors_for_sweeps
 from ipfx.stimulus_protocol_analysis import LongSquareAnalysis
-from ipfx.ephys_data_set import Sweep, SweepSet
+from ipfx.sweep import Sweep, SweepSet
+from ipfx.chirp_features import extract_chirp_features
 from .pipeline_module import MultipatchPipelineModule
 from .experiment import ExperimentPipelineModule
 from .dataset import DatasetPipelineModule
-from ...nwb_recordings import get_lp_sweeps, get_pulse_times, get_db_recording
+from ...nwb_recordings import get_intrinsic_recording_dict, get_pulse_times
 
 SPIKE_FEATURES = [
     'upstroke_downstroke_ratio',
@@ -32,7 +33,6 @@ class IntrinsicPipelineModule(MultipatchPipelineModule):
 
     @classmethod
     def create_db_entries(cls, job, session):
-        errors = []
         db = job['database']
         job_id = job['job_id']
 
@@ -41,83 +41,105 @@ class IntrinsicPipelineModule(MultipatchPipelineModule):
         nwb = expt.data
         if nwb is None:
             raise Exception('No NWB data for this experiment')
-        sweeps = nwb.contents
 
         n_cells = len(expt.cell_list)
-        ipfx_fail = 0
+        errors = []
         for cell in expt.cell_list:
             dev_id = cell.electrode.device_id
-            target_v, if_curve = get_lp_sweeps(sweeps, dev_id)
-            lp_sweeps = target_v + if_curve
-            if len(lp_sweeps) == 0:
-                errors.append('No long pulse sweeps for cell %s' % cell.ext_id)
-                continue
-            recs = [rec[dev_id] for rec in lp_sweeps]
-            min_pulse_dur = np.inf
-            sweep_list = []
-            for rec in recs:
-                if rec.clamp_mode != 'ic':
-                    continue
-
-                db_rec = get_db_recording(expt, rec)
-                if db_rec is None or db_rec.patch_clamp_recording.qc_pass is False:
-                    continue
-
-                pulse_times = get_pulse_times(rec)
-                if pulse_times is None:
-                    continue
-                
-                # pulses may have different durations as well, so we just use the smallest duration
-                start, end = pulse_times
-                min_pulse_dur = min(min_pulse_dur, end-start)
-                
-                sweep = MPSweep(rec, -start)
-                if sweep is None:
-                    continue
-                sweep_list.append(sweep)
-            
-            if len(sweep_list) == 0:
-                errors.append('No sweeps passed qc for cell %s' % cell.ext_id)
-                continue
-
-            sweep_set = SweepSet(sweep_list)    
-            spx, spfx = extractors_for_sweeps(sweep_set, start=0, end=min_pulse_dur)
-            lsa = LongSquareAnalysis(spx, spfx, subthresh_min_amp=-200)
-            
-            try:
-                analysis = lsa.analyze(sweep_set)
-            except Exception as exc:
-                errors.append('Error running IPFX analysis for cell %s: %s' % (cell.ext_id, str(exc)))
-                ipfx_fail += 1
-                continue
-
-            if ipfx_fail == n_cells and n_cells > 1:
-                raise Exception('All cells failed IPFX analysis')
-                continue
-            
-            spike_features = lsa.mean_features_first_spike(analysis['spikes_set'], features_list=SPIKE_FEATURES)
-            rheo = analysis['rheobase_i'] * 1e-9 #unscale from pA
-            fi_slope = analysis['fi_fit_slope'] * 1e-9 #unscale from pA
-            input_r = analysis['input_resistance'] * 1e6 #unscale from MOhm
-            sag = analysis['sag']
-            avg_rate = np.mean(analysis['spiking_sweeps'].avg_rate)
-            adapt = np.mean(analysis['spiking_sweeps'].adapt)
-            
-            results = {
-                'rheobase': rheo,
-                'fi_slope': fi_slope,
-                'input_resistance': input_r,
-                'sag': sag,
-                'avg_firing_rate': np.nan,
-                'adaptation_index': adapt,
-            }
-            results.update(spike_features)
-
+            recording_dict = get_intrinsic_recording_dict(expt, dev_id)
+            lp_recs = recording_dict['If_Curve'] + recording_dict['TargetV']
+            results = {}
+            lp_results, error = IntrinsicPipelineModule.get_long_square_features(lp_recs, cell_id=cell.ext_id)
+            errors += error
+            chirp_results, error = IntrinsicPipelineModule.get_chirp_features(recording_dict['Chirp'], cell_id=cell.ext_id)
+            errors += error
             # Write new record to DB
-            conn = db.Intrinsic(cell_id=cell.id, **results)
+            conn = db.Intrinsic(cell_id=cell.id, **lp_results, **chirp_results)
             session.add(conn)
+        
+        if len(errors) == 2*n_cells and n_cells > 1:
+            raise Exception('All cells failed IPFX analysis')
 
         return errors
+
+    @staticmethod
+    def get_chirp_features(recordings, cell_id=''):
+        errors = []
+        if len(recordings) == 0:
+            errors.append('No chirp sweeps for cell %s' % cell_id)
+            return {}, errors
+             
+        sweep_list = []
+        for rec in recordings:
+            sweep = MPSweep(rec)
+            if sweep is not None:
+                sweep_list.append(sweep)
+        
+        if len(sweep_list) == 0:
+            errors.append('No sweeps passed qc for cell %s' % cell_id)
+            return {}, errors
+
+        sweep_set = SweepSet(sweep_list) 
+        all_chirp_features = extract_chirp_features(sweep_set)
+        results = {
+            'chirp_peak_freq': all_chirp_features['peak_freq'],
+            'chirp_3db_freq': all_chirp_features['3db_freq'],
+            'chirp_peak_ratio': all_chirp_features['peak_ratio'],
+        }
+        return results, errors
+
+    @staticmethod
+    def get_long_square_features(recordings, cell_id=''):
+        errors = []
+        if len(recordings) == 0:
+            errors.append('No long pulse sweeps for cell %s' % cell_id)
+            return {}, errors
+
+        min_pulse_dur = np.inf
+        sweep_list = []
+        for rec in recordings:
+            pulse_times = get_pulse_times(rec)
+            if pulse_times is None:
+                continue
+            
+            # pulses may have different durations as well, so we just use the smallest duration
+            start, end = pulse_times
+            min_pulse_dur = min(min_pulse_dur, end-start)
+            
+            sweep = MPSweep(rec, -start)
+            if sweep is not None:
+                sweep_list.append(sweep)
+        
+        if len(sweep_list) == 0:
+            errors.append('No sweeps passed qc for cell %s' % cell_id)
+            return {}, errors
+
+        sweep_set = SweepSet(sweep_list)    
+        spx, spfx = extractors_for_sweeps(sweep_set, start=0, end=min_pulse_dur)
+        lsa = LongSquareAnalysis(spx, spfx, subthresh_min_amp=-200)
+        
+        try:
+            analysis = lsa.analyze(sweep_set)
+        except Exception as exc:
+            errors.append('Error running IPFX analysis for cell %s: %s' % (cell_id, str(exc)))
+            return {}, errors
+        
+        spike_features = lsa.mean_features_first_spike(analysis['spikes_set'], features_list=SPIKE_FEATURES)
+        rheo = analysis['rheobase_i'] * 1e-9 #unscale from pA
+        fi_slope = analysis['fi_fit_slope'] * 1e-9 #unscale from pA
+        input_r = analysis['input_resistance'] * 1e6 #unscale from MOhm
+        sag = analysis['sag']
+        adapt = analysis['spiking_sweeps'].adapt.mean()
+        
+        results = {
+            'rheobase': rheo,
+            'fi_slope': fi_slope,
+            'input_resistance': input_r,
+            'sag': sag,
+            'adaptation_index': adapt,
+        }
+        results.update(spike_features)
+        return results, errors
 
     def job_records(self, job_ids, session):
         """Return a list of records associated with a list of job IDs.
@@ -134,7 +156,7 @@ class IntrinsicPipelineModule(MultipatchPipelineModule):
 class MPSweep(Sweep):
     """Adapter for neuroanalysis.Recording => ipfx.Sweep
     """
-    def __init__(self, rec, t0):
+    def __init__(self, rec, t0=0):
         # pulses may have different start times, so we shift time values to make all pulses start at t=0
         pri = rec['primary'].copy(t0=t0)
         cmd = rec['command'].copy()
@@ -142,6 +164,7 @@ class MPSweep(Sweep):
         v = pri.data * 1e3  # convert to mV
         holding = [i for i in rec.stimulus.items if i.description=='holding current']
         if len(holding) == 0:
+            # TODO: maybe log this error
             return None
         holding = holding[0].amplitude
         i = (cmd.data - holding) * 1e12   # convert to pA with holding current removed
