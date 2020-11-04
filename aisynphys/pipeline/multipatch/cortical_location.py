99100d0e8783c9aea0ef47ef60290bcfcab7cfea
Tom Chartrand <tom.chartrand@alleninstitute.org>
2020-11-04 11:19:24 -0800

    Use layer bounds not pia/wm for depth calculation
    Infers thickness of missing layers from species averages.
    Also moves the average direction calculation to pipeline module.

--- aisynphys/pipeline/multipatch/cortical_location.py
+++ aisynphys/pipeline/multipatch/cortical_location.py
@@ -47,7 +47,8 @@ class CortexLocationPipelineModule(DatabasePipelineModule):
             errors.append("No cell coordinates found for cell cluster.")
             return errors
         
-        results, cell_errors = get_depths_slice(image_series_id, soma_centers, 
+        results, cell_errors = get_depths_slice(image_series_id, soma_centers,
+                                                species=slice_entry.species,
                                                 resolution=image_series_resolution)
         if len(results)==0:
             errors.append("No cells passed depth calculation.")
@@ -57,32 +58,37 @@ class CortexLocationPipelineModule(DatabasePipelineModule):
         missed_cell_count = 0
         for cell in expt.cell_list:
             specimen_id = cell.meta.get('lims_specimen_id')
+            if specimen_id not in soma_centers:
+                continue
             if specimen_id not in results:
                 missed_cell_count += 1
-                continue
-            cell_results = results[specimen_id]
-            loc_entry = db.CorticalCellLocation(
-                layer=cell_results["layer"],
-                distance_to_pia=cell_results["absolute_depth"]*1e-6,
-                distance_to_wm=cell_results["wm_distance"]*1e-6,
-                fractional_depth=cell_results["normalized_depth"],
-                layer_depth=cell_results["layer_depth"]*1e-6,
-                fractional_layer_depth=cell_results["normalized_layer_depth"],
-                position=list(cell_results["position"]*1e-6),
-                cell=cell,
-            )
+                loc_entry = db.CorticalCellLocation(
+                    cell=cell,
+                    position=soma_centers[specimen_id]
+                )
+            else:
+                cell_results = results[specimen_id]
+                loc_entry = db.CorticalCellLocation(
+                    layer=cell_results["layer"],
+                    distance_to_pia=cell_results["absolute_depth"]*1e-6,
+                    distance_to_wm=cell_results["wm_distance"]*1e-6,
+                    fractional_depth=cell_results["normalized_depth"],
+                    layer_depth=cell_results["layer_depth"]*1e-6,
+                    fractional_layer_depth=cell_results["normalized_layer_depth"],
+                    position=list(cell_results["position"]*1e-6),
+                    cell=cell,
+                )
             session.add(loc_entry)
         
         if missed_cell_count > 0:
-            errors.append(f"{missed_cell_count}/{len(expt.cell_list)} cells failed depth calculation.")
+            errors.append(f"{missed_cell_count}/{len(soma_centers)} cells failed depth calculation.")
             errors.extend(cell_errors)
 
         if 'mean' not in results:
             errors.append("Depth calculation for cell cluster mean failed. No pair distances available.")
             return errors
 
-        mean_results = results['mean']
-        pia_direction = mean_results['pia_direction']
+        pia_direction = np.stack([res['pia_direction'] for res in results]).mean(axis=0)
 
         for pair in expt.pair_list:
             pre_id = pair.pre_cell.meta.get('lims_specimen_id')
@@ -91,6 +97,8 @@ class CortexLocationPipelineModule(DatabasePipelineModule):
                 d12_lat, d12_vert = get_pair_distances(pair, pia_direction)
                 pair.lateral_distance = d12_lat
                 pair.vertical_distance = d12_vert
+                
+        return errors
 
     def job_records(self, job_ids, session):
         """Return a list of records associated with a list of job IDs.
