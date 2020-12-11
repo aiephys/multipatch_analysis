4efcdd9935255482ed8b9df163f943e50c4c20e4
ALLENINST\stephanies <stephanies@alleninstitute.org>
2020-12-10 16:50:06 -0800

    syntax

--- aisynphys/metadata_submission.py
+++ aisynphys/metadata_submission.py
@@ -292,7 +292,7 @@ class ExperimentMetadataSubmission(object):
                 last_monday  = site_date - timedelta(days=site_date.weekday())
                 expected_plate_date = [last_monday]
                 # at times of reduced staffing the plate schedule can change, add other acceptable plate dates here
-                expected_plate_date.append(last_monday + timedelta(days=7) # "next Monday"
+                expected_plate_date.append(last_monday + timedelta(days=7)) # "next Monday"
                 if abs((expected_plate_date[0] - plate_date).total_seconds()) > 7*24*3600:
                     # error if more than a week out of sync
                     errors.append("Histology well date is %s%s%s; expected %s: %s" % (yy, mm, dd, expected_plate_date.strftime('%y%m%d'), lims_edit_href))
