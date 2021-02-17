75ea1b6c7e98a7798e9d8874de567a0bacb8bc89
Luke Campagnola <lukec@alleninstitute.org>
2021-02-16 17:32:26 -0800

    re-enable synapse prediction pipeline module

--- aisynphys/pipeline/multipatch/__init__.py
+++ aisynphys/pipeline/multipatch/__init__.py
@@ -29,7 +29,7 @@ class MultipatchPipeline(Pipeline):
         SynapsePipelineModule,
         GapJunctionPipelineModule,
         PulseResponsePipelineModule,
-        #SynapsePredictionPipelineModule,
+        SynapsePredictionPipelineModule,
         RestingStatePipelineModule,
         DynamicsPipelineModule,
         CortexLocationPipelineModule
