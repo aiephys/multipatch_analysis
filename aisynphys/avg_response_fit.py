82688912bb185c7dcb469638c1f016a676bd3788
Luke Campagnola <lukec@alleninstitute.org>
2021-03-20 19:40:00 -0700

    consolidate fitting code

--- aisynphys/avg_response_fit.py
+++ aisynphys/avg_response_fit.py
@@ -1,15 +1,11 @@
 # coding: utf8
-import sys
-from collections import OrderedDict
 import numpy as np
+from pyqtgraph.debug import Profiler
 import pyqtgraph as pg
-from neuroanalysis.data import TSeries, TSeriesList
-from neuroanalysis.baseline import float_mode
-from neuroanalysis.fitting import Psp, StackedPsp
-from aisynphys.database import default_db as db
+from neuroanalysis.fitting import fit_psp
 import aisynphys.data.data_notes_db as notes_db
-from aisynphys.qc import spike_qc
-from aisynphys.fitting import fit_avg_pulse_response
+from aisynphys.database import default_db as db
+from aisynphys.data import PulseResponseList
 
 
 def get_pair_avg_fits(pair, session, notes_session=None, ui=None, max_ind_freq=50):
@@ -226,3 +222,59 @@ def check_fit_qc_pass(fit_result, expected_params, clamp_mode):
             failures.append('%s error too large (%s != %s)' % (k, v1, v2))
 
     return len(failures) == 0, failures
+
+
+def fit_avg_pulse_response(pulse_response_list, latency_window, sign, init_params=None, ui=None):
+    """Generate PSP fit parameters for a list of pulse responses, possibly correcting
+    for crosstalk artifacts and gap junctional current during the presynaptic stimulus.
+    
+    Parameters
+    ----------
+    pulse_response_list : list
+        A list of PulseResponse instances to be time-aligned, averaged, and fit.
+    latency_window : (float, float)
+        Beginning and end times of a window over which to search for a synaptic response,
+        relative to the spike time.
+    sign : int
+        +1, -1, or 0 indicating the expected sign of the response (see neuroanalysis.fitting.fit_psp)
+    
+    Returns
+    -------
+    fit : lmfit ModelResult
+        The resulting PSP fit
+    average : TSeries
+        The averaged pulse response data
+    
+    """
+    prof = Profiler(disabled=True, delayed=False)
+    pair = pulse_response_list[0].pair
+    clamp_mode = pulse_response_list[0].recording.patch_clamp_recording.clamp_mode
+
+    # make a list of spike-aligned postsynaptic tseries
+    tsl = PulseResponseList(pulse_response_list).post_tseries(align='spike', bsub=True)
+    prof('make tseries list')
+    
+    # average all together
+    average = tsl.mean()
+    prof('average')
+        
+    # start with even weighting
+    weight = np.ones(len(average))
+    
+    # boost weight around PSP onset
+    onset_start_idx = average.index_at(latency_window[0])
+    onset_stop_idx = average.index_at(latency_window[1] + 4e-3) 
+    weight[onset_start_idx:onset_stop_idx] = 3.0
+    
+    # decide whether to mask out crosstalk artifact
+    pre_id = int(pair.pre_cell.electrode.ext_id)
+    post_id = int(pair.post_cell.electrode.ext_id)
+    if abs(pre_id - post_id) < 3:
+        # nearby electrodes; mask out crosstalk
+        pass
+    prof('weights')
+
+    fit = fit_psp(average, search_window=latency_window, clamp_mode=clamp_mode, sign=sign, baseline_like_psp=True, init_params=init_params, fit_kws={'weights': weight})
+    prof('fit')
+    
+    return fit, average
